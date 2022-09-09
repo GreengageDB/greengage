open-gpdb/gpdb

commit c4a3ce097f554a15ef489630928f03d19e129b39

Fix crash when the qual under DynamicTableScan has a subplan
Consider the query below:

```
test=# explain select * from foo, jazz where foo.c = jazz.e and jazz.f = 10 and a in (select b+1 from bar);
						 QUERY PLAN
--------------------------------------------------------------------------------------------------------------------
Gather Motion 3:1  (slice4; segments: 3)  (cost=0.00..1324469.30 rows=1 width=20)
->  Hash Join  (cost=0.00..1324469.30 rows=1 width=20)
     Hash Cond: foo.c = jazz.e
     ->  Dynamic Table Scan on foo (dynamic scan id: 1)  (cost=0.00..1324038.29 rows=34 width=12)
	   Filter: a = (b + 1) AND ((subplan))
	   SubPlan 1
	     ->  Materialize  (cost=0.00..431.00 rows=1 width=4)
		   ->  Broadcast Motion 1:3  (slice2)  (cost=0.00..431.00 rows=3 width=4)
			 ->  Limit  (cost=0.00..431.00 rows=1 width=4)
			       ->  Gather Motion 3:1  (slice1; segments: 3)  (cost=0.00..431.00 rows=1 width=4)
				     ->  Limit  (cost=0.00..431.00 rows=1 width=4)
					   ->  Table Scan on bar  (cost=0.00..431.00 rows=34 width=4)
     ->  Hash  (cost=100.00..100.00 rows=34 width=4)
	   ->  Partition Selector for foo (dynamic scan id: 1)  (cost=10.00..100.00 rows=34 width=4)
		 ->  Broadcast Motion 3:3  (slice3; segments: 3)  (cost=0.00..431.00 rows=1 width=8)
		       ->  Table Scan on jazz  (cost=0.00..431.00 rows=1 width=8)
			     Filter: f = 10
Optimizer status: PQO version 3.65.0
(18 rows)
```

Previously, since the subplan was in a qual, we did not populate the
qual properly when executing a dynamic table scan node. Thus the
subplan attribute in PlanState of the dynamic table scan was incorrectly
set to NULL, causing a later crash.

We now populate this similarly to how we do it for dynamic index/bitmap
scans.

Co-authored-by: Sambitesh Dash <sdash@pivotal.io>
Co-authored-by: Chris Hajas <chajas@pivotal.io>
Co-authored-by: Ashuka Xue <axue@pivotal.io>

(cherry picked from commit 81a671f6c560f8424d3589c1ccca23805090a266)

src/backend/executor/nodeDynamicSeqscan.c
@@ -49,6 +49,14 @@ ExecInitDynamicSeqScan(DynamicSeqScan *node, EState *estate, int eflags)
 
 	state->scan_state = SCAN_INIT;
 
+	/* Initialize child expressions. This is needed to find subplans. */
+	state->ss.ps.qual = (List *)
+		ExecInitExpr((Expr *) node->seqscan.plan.qual,
+					 (PlanState *) state);
+	state->ss.ps.targetlist = (List *)
+		ExecInitExpr((Expr *) node->seqscan.plan.targetlist,
+					 (PlanState *) state);
+
 	/* Initialize result tuple type. */
 	ExecInitResultTupleSlot(estate, &state->ss.ps);
 	ExecAssignResultTypeFromTL(&state->ss.ps);

src/test/regress/expected/gporca.out
@@ -11296,3 +11296,90 @@ SELECT * FROM touter LEFT JOIN tinnerbtree ON touter.a = tinnerbtree.a AND tinne
   8 | 2 |   |  
 (10 rows)
 
+-- test subplan in a qual under dynamic scan
+CREATE TABLE ds_part ( a INT, b INT, c INT) PARTITION BY RANGE(c)( START(1) END (10) EVERY (2), DEFAULT PARTITION deflt);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+NOTICE:  CREATE TABLE will create partition "ds_part_1_prt_deflt" for table "ds_part"
+NOTICE:  CREATE TABLE will create partition "ds_part_1_prt_2" for table "ds_part"
+NOTICE:  CREATE TABLE will create partition "ds_part_1_prt_3" for table "ds_part"
+NOTICE:  CREATE TABLE will create partition "ds_part_1_prt_4" for table "ds_part"
+NOTICE:  CREATE TABLE will create partition "ds_part_1_prt_5" for table "ds_part"
+NOTICE:  CREATE TABLE will create partition "ds_part_1_prt_6" for table "ds_part"
+CREATE TABLE non_part1 (c INT);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'c' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+CREATE TABLE non_part2 (e INT, f INT);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'e' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+INSERT INTO ds_part SELECT i, i, i FROM generate_series (1, 1000)i; 
+INSERT INTO non_part1 SELECT i FROM generate_series(1, 100)i; 
+INSERT INTO non_part2 SELECT i, i FROM generate_series(1, 100)i;
+SET optimizer_enforce_subplans TO ON;
+analyze ds_part;
+analyze non_part1;
+analyze non_part2;
+SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
+ a | b | c | e | f 
+---+---+---+---+---
+(0 rows)
+
+explain SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
+                                                  QUERY PLAN                                                   
+---------------------------------------------------------------------------------------------------------------
+ Gather Motion 3:1  (slice3; segments: 3)  (cost=10000000004.33..10000000039.13 rows=7 width=20)
+   ->  Nested Loop Semi Join  (cost=10000000004.33..10000000039.13 rows=3 width=20)
+         ->  Hash Join  (cost=4.33..28.40 rows=2 width=20)
+               Hash Cond: (ds_part_1_prt_deflt.c = non_part2.e)
+               ->  Append  (cost=0.00..24.00 rows=2 width=12)
+                     ->  Result  (cost=0.00..17.87 rows=1 width=12)
+                           One-Time Filter: PartSelected
+                           ->  Seq Scan on ds_part_1_prt_deflt  (cost=0.00..17.87 rows=1 width=12)
+                                 Filter: (a = (b + 1))
+                     ->  Result  (cost=0.00..2.03 rows=1 width=12)
+                           One-Time Filter: PartSelected
+                           ->  Seq Scan on ds_part_1_prt_2  (cost=0.00..2.03 rows=1 width=12)
+                                 Filter: (a = (b + 1))
+                     ->  Result  (cost=0.00..1.03 rows=1 width=12)
+                           One-Time Filter: PartSelected
+                           ->  Seq Scan on ds_part_1_prt_3  (cost=0.00..1.03 rows=1 width=12)
+                                 Filter: (a = (b + 1))
+                     ->  Result  (cost=0.00..1.03 rows=1 width=12)
+                           One-Time Filter: PartSelected
+                           ->  Seq Scan on ds_part_1_prt_4  (cost=0.00..1.03 rows=1 width=12)
+                                 Filter: (a = (b + 1))
+                     ->  Result  (cost=0.00..1.03 rows=1 width=12)
+                           One-Time Filter: PartSelected
+                           ->  Seq Scan on ds_part_1_prt_5  (cost=0.00..1.03 rows=1 width=12)
+                                 Filter: (a = (b + 1))
+                     ->  Result  (cost=0.00..1.01 rows=1 width=12)
+                           One-Time Filter: PartSelected
+                           ->  Seq Scan on ds_part_1_prt_6  (cost=0.00..1.01 rows=1 width=12)
+                                 Filter: (a = (b + 1))
+               ->  Hash  (cost=4.29..4.29 rows=1 width=8)
+                     ->  Partition Selector for ds_part (dynamic scan id: 1)  (cost=0.00..4.29 rows=1 width=8)
+                           Filter: non_part2.e
+                           ->  Broadcast Motion 3:3  (slice1; segments: 3)  (cost=0.00..4.29 rows=1 width=8)
+                                 ->  Seq Scan on non_part2  (cost=0.00..4.25 rows=1 width=8)
+                                       Filter: (f = 10)
+         ->  Materialize  (cost=0.00..9.50 rows=100 width=0)
+               ->  Broadcast Motion 3:3  (slice2; segments: 3)  (cost=0.00..8.00 rows=100 width=0)
+                     ->  Seq Scan on non_part1  (cost=0.00..4.00 rows=34 width=0)
+ Optimizer: Postgres query optimizer
+(39 rows)
+
+SELECT *, a IN ( SELECT b + 1 FROM non_part1) FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b FROM non_part1);
+ a  | b  | c  | e  | f  | ?column? 
+----+----+----+----+----+----------
+ 10 | 10 | 10 | 10 | 10 | f
+(1 row)
+
+CREATE INDEX ds_idx ON ds_part(a);
+analyze ds_part;
+SELECT *, a IN ( SELECT b + 1 FROM non_part1) FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b FROM non_part1);
+ a  | b  | c  | e  | f  | ?column? 
+----+----+----+----+----+----------
+ 10 | 10 | 10 | 10 | 10 | f
+(1 row)
+
+RESET optimizer_enforce_subplans;

GreengageDB/greengage

commit c491b58bf09429409bffefb83007c97bfc5e0a52

Fix failure when DynamicSeqScan has a subPlan (#368)
If Query plan contains SubPlan under DynamicSeqScan node we got a situation:
macro "Insist" fails at condition "rsi->pstype == planstate->type".
We have duplicate messages for Materialize and Broadcast Motion nodes in messages
from QEs to QD. This is because QE executing slice initializes two identical subplans
for DynamicSeqScan node. The fist subplan item is initialized at executor start.
The second one - at execution of DynamicSeqScan node in initNextTableToScan.
This duplication was caused by commit c4a3ce097f554a15ef489630928f03d19e129b39
that added initialization of quals and targetlist at executor start alongside
initialization at running stage.

Another issue with second subplan initialization is that this subplan structure is allocated in
temporary memory context partitionMemoryContext. In particular, after targetlist
remapping we end up to dangling pointer in list of subplans.

Also there is the same problem at DynamicBitmapHeapscan.

src/backend/executor/nodeDynamicBitmapHeapscan.c
@@ -55,14 +55,17 @@ ExecInitDynamicBitmapHeapScan(DynamicBitmapHeapScan *node, EState *estate, int e
 
 	state->scan_state = SCAN_INIT;
 
+	state->ss.ps.qual = (List *) ExecInitExpr((Expr *) node->bitmapheapscan.scan.plan.qual,
+											   (PlanState *) state);
+	state->ss.ps.targetlist = (List *) ExecInitExpr((Expr *) node->bitmapheapscan.scan.plan.targetlist,
+													 (PlanState *) state);
+
 	/* Initialize result tuple type. */
 	ExecInitResultTupleSlot(estate, &state->ss.ps);
 	ExecAssignResultTypeFromTL(&state->ss.ps);
 
 	reloid = getrelid(node->bitmapheapscan.scan.scanrelid, estate->es_range_table);
 	Assert(OidIsValid(reloid));
-
-	state->firstPartition = true;
 
 	/* lastRelOid is used to remap varattno for heterogeneous partitions */
 	state->lastRelOid = reloid;
@@ -170,30 +173,8 @@ initNextTableToScan(DynamicBitmapHeapScanState *node)
 		 * the new varnos correspond to
 		 */
 		node->lastRelOid = currentPartitionOid;
-	}
-
-	/*
-	 * For the very first partition, the targetlist of planstate is set to null. So, we must
-	 * initialize quals and targetlist, regardless of remapping requirements. For later
-	 * partitions, we only initialize quals and targetlist if a column re-mapping is necessary.
-	 */
-	if (attMap || node->firstPartition)
-	{
-		node->firstPartition = false;
-		MemoryContextReset(node->partitionMemoryContext);
-		MemoryContext oldCxt = MemoryContextSwitchTo(node->partitionMemoryContext);
-
-		/* Initialize child expressions */
-		scanState->ps.qual = (List *) ExecInitExpr((Expr *) scanState->ps.plan->qual,
-												   (PlanState *) scanState);
-		scanState->ps.targetlist = (List *) ExecInitExpr((Expr *) scanState->ps.plan->targetlist,
-														 (PlanState *) scanState);
-
-		MemoryContextSwitchTo(oldCxt);
-	}
-
-	if (attMap)
 		pfree(attMap);
+	}
 
 	DynamicScan_SetTableOid(&node->ss, currentPartitionOid);
 

src/backend/executor/nodeDynamicSeqscan.c
@@ -67,7 +67,6 @@ ExecInitDynamicSeqScan(DynamicSeqScan *node, EState *estate, int eflags)
 	reloid = getrelid(node->seqscan.scanrelid, estate->es_range_table);
 	Assert(OidIsValid(reloid));
 
-	state->firstPartition = true;
 
 	/* lastRelOid is used to remap varattno for heterogeneous partitions */
 	state->lastRelOid = reloid;
@@ -166,30 +165,8 @@ initNextTableToScan(DynamicSeqScanState *node)
 		 * the new varnos correspond to
 		 */
 		node->lastRelOid = *pid;
-	}
-
-	/*
-	 * For the very first partition, the targetlist of planstate is set to null. So, we must
-	 * initialize quals and targetlist, regardless of remapping requirements. For later
-	 * partitions, we only initialize quals and targetlist if a column re-mapping is necessary.
-	 */
-	if (attMap || node->firstPartition)
-	{
-		node->firstPartition = false;
-		MemoryContextReset(node->partitionMemoryContext);
-		MemoryContext oldCxt = MemoryContextSwitchTo(node->partitionMemoryContext);
-
-		/* Initialize child expressions */
-		scanState->ps.qual = (List *) ExecInitExpr((Expr *) scanState->ps.plan->qual,
-												   (PlanState *) scanState);
-		scanState->ps.targetlist = (List *) ExecInitExpr((Expr *) scanState->ps.plan->targetlist,
-														 (PlanState *) scanState);
-
-		MemoryContextSwitchTo(oldCxt);
-	}
-
-	if (attMap)
 		pfree(attMap);
+	}
 
 	DynamicScan_SetTableOid(&node->ss, *pid);
 

src/include/nodes/execnodes.h
@@ -1966,11 +1966,6 @@ typedef struct DynamicBitmapHeapScanState
 	bool		shouldCallHashSeqTerm;
 
 	/*
-	 * The first partition requires initialization of expression states,
-	 * such as qual and targetlist, regardless of whether we need to re-map varattno
-	 */
-	bool		firstPartition;
-	/*
 	 * lastRelOid is the last relation that corresponds to the
 	 * varattno mapping of qual and target list. Each time we open a new partition, we will
 	 * compare the last relation with current relation by using varattnos_map()
@@ -2241,11 +2236,6 @@ typedef struct DynamicSeqScanState
 	 */
 	bool		shouldCallHashSeqTerm;
 
-	/*
-	 * The first partition requires initialization of expression states,
-	 * such as qual and targetlist, regardless of whether we need to re-map varattno
-	 */
-	bool		firstPartition;
 	/*
 	 * lastRelOid is the last relation that corresponds to the
 	 * varattno mapping of qual and target list. Each time we open a new partition, we will

src/test/regress/expected/gporca.out
@@ -11565,7 +11565,7 @@ SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f =
 ---+---+---+---+---
 (0 rows)
 
-explain SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
+explain analyze SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
                                                   QUERY PLAN                                                   
 ---------------------------------------------------------------------------------------------------------------
  Gather Motion 3:1  (slice3; segments: 3)  (cost=10000000004.33..10000000039.13 rows=7 width=20)

src/test/regress/expected/gporca_optimizer.out
@@ -11686,7 +11686,7 @@ SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f =
 ---+---+---+---+---
 (0 rows)
 
-explain SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
+explain analyze SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
                                                      QUERY PLAN                                                     
 --------------------------------------------------------------------------------------------------------------------
  Gather Motion 3:1  (slice4; segments: 3)  (cost=0.00..1324481.28 rows=1000 width=20)

src/test/regress/expected/qp_dropped_cols.out
@@ -16186,6 +16186,91 @@ select * from multi_part_mid_bitmap where date = '2017-02-05' and region = 'usa'
 ----------+------+--------+--------
 (0 rows)
 
+-- Exchange partitions with dropped columns and check subplan with attribute remapping into dynamicSeqscan
+-- start_ignore
+DROP TABLE if exists ds_main;
+NOTICE:  table "ds_main" does not exist, skipping
+DROP TABLE if exists ds_part1;
+NOTICE:  table "ds_part1" does not exist, skipping
+DROP TABLE if exists non_part1;
+NOTICE:  table "non_part1" does not exist, skipping
+DROP TABLE if exists non_part2;
+NOTICE:  table "non_part2" does not exist, skipping
+-- end_ignore
+CREATE TABLE ds_main ( a INT, b INT, c INT) PARTITION BY RANGE(c)( START(1) END (10) EVERY (2), DEFAULT PARTITION deflt);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_deflt" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_2" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_3" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_4" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_5" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_6" for table "ds_main"
+CREATE TABLE ds_part1 (a int, a1 int, b int, c int);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+CREATE TABLE non_part1 (c INT);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'c' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+CREATE TABLE non_part2 (e INT, f INT);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'e' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+SET optimizer_enforce_subplans TO ON;
+-- drop columns to get attribute remapping
+ALTER TABLE ds_part1 drop column a1;
+ALTER TABLE ds_main exchange partition for (1) with table ds_part1;
+INSERT INTO non_part1 SELECT i FROM generate_series(1, 1)i;
+INSERT INTO non_part2 SELECT i, i + 1 FROM generate_series(1, 10)i;
+INSERT INTO ds_main SELECT i, i + 1, i + 2 FROM generate_series (1, 1000)i;
+analyze ds_main;
+analyze non_part1;
+analyze non_part2;
+EXPLAIN (costs off) SELECT * FROM ds_main, non_part2 WHERE ds_main.c = non_part2.e AND a IN ( SELECT a FROM non_part1) order by a;
+                               QUERY PLAN                               
+------------------------------------------------------------------------
+ Gather Motion 3:1  (slice3; segments: 3)
+   Merge Key: ds_main_1_prt_deflt.a
+   ->  Sort
+         Sort Key: ds_main_1_prt_deflt.a
+         ->  Nested Loop Semi Join
+               ->  Hash Join
+                     Hash Cond: (ds_main_1_prt_deflt.c = non_part2.e)
+                     ->  Redistribute Motion 3:3  (slice1; segments: 3)
+                           Hash Key: ds_main_1_prt_deflt.c
+                           ->  Append
+                                 ->  Seq Scan on ds_main_1_prt_deflt
+                                       Filter: (a = a)
+                                 ->  Seq Scan on ds_main_1_prt_3
+                                       Filter: (a = a)
+                                 ->  Seq Scan on ds_main_1_prt_4
+                                       Filter: (a = a)
+                                 ->  Seq Scan on ds_main_1_prt_5
+                                       Filter: (a = a)
+                                 ->  Seq Scan on ds_main_1_prt_6
+                                       Filter: (a = a)
+                                 ->  Seq Scan on ds_main_1_prt_2
+                                       Filter: (a = a)
+                     ->  Hash
+                           ->  Seq Scan on non_part2
+               ->  Materialize
+                     ->  Broadcast Motion 3:3  (slice2; segments: 3)
+                           ->  Seq Scan on non_part1
+ Optimizer: Postgres query optimizer
+(28 rows)
+
+SELECT * FROM ds_main, non_part2 WHERE ds_main.c = non_part2.e AND a IN ( SELECT a FROM non_part1) order by a;
+ a | b | c  | e  | f  
+---+---+----+----+----
+ 1 | 2 |  3 |  3 |  4
+ 2 | 3 |  4 |  4 |  5
+ 3 | 4 |  5 |  5 |  6
+ 4 | 5 |  6 |  6 |  7
+ 5 | 6 |  7 |  7 |  8
+ 6 | 7 |  8 |  8 |  9
+ 7 | 8 |  9 |  9 | 10
+ 8 | 9 | 10 | 10 | 11
+(8 rows)
+
 -- As of this writing, pg_dump creates an invalid dump for some of the tables
 -- here. See https://github.com/greenplum-db/gpdb/issues/3598. So we must drop
 -- the tables, or the pg_upgrade test fails.

src/test/regress/expected/qp_dropped_cols_optimizer.out
@@ -16108,6 +16108,82 @@ select * from multi_part_mid_bitmap where date = '2017-02-05' and region = 'usa'
 ----------+------+--------+--------
 (0 rows)
 
+-- Exchange partitions with dropped columns and check subplan with attribute remapping into dynamicSeqscan
+-- start_ignore
+DROP TABLE if exists ds_main;
+NOTICE:  table "ds_main" does not exist, skipping
+DROP TABLE if exists ds_part1;
+NOTICE:  table "ds_part1" does not exist, skipping
+DROP TABLE if exists non_part1;
+NOTICE:  table "non_part1" does not exist, skipping
+DROP TABLE if exists non_part2;
+NOTICE:  table "non_part2" does not exist, skipping
+-- end_ignore
+CREATE TABLE ds_main ( a INT, b INT, c INT) PARTITION BY RANGE(c)( START(1) END (10) EVERY (2), DEFAULT PARTITION deflt);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_deflt" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_2" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_3" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_4" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_5" for table "ds_main"
+NOTICE:  CREATE TABLE will create partition "ds_main_1_prt_6" for table "ds_main"
+CREATE TABLE ds_part1 (a int, a1 int, b int, c int);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+CREATE TABLE non_part1 (c INT);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'c' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+CREATE TABLE non_part2 (e INT, f INT);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'e' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+SET optimizer_enforce_subplans TO ON;
+-- drop columns to get attribute remapping
+ALTER TABLE ds_part1 drop column a1;
+ALTER TABLE ds_main exchange partition for (1) with table ds_part1;
+INSERT INTO non_part1 SELECT i FROM generate_series(1, 1)i;
+INSERT INTO non_part2 SELECT i, i + 1 FROM generate_series(1, 10)i;
+INSERT INTO ds_main SELECT i, i + 1, i + 2 FROM generate_series (1, 1000)i;
+analyze ds_main;
+analyze non_part1;
+analyze non_part2;
+EXPLAIN (costs off) SELECT * FROM ds_main, non_part2 WHERE ds_main.c = non_part2.e AND a IN ( SELECT a FROM non_part1) order by a;
+                                      QUERY PLAN                                      
+--------------------------------------------------------------------------------------
+ Gather Motion 3:1  (slice4; segments: 3)
+   Merge Key: ds_main.a
+   ->  Sort
+         Sort Key: ds_main.a
+         ->  Hash Join
+               Hash Cond: (ds_main.c = non_part2.e)
+               ->  Dynamic Seq Scan on ds_main (dynamic scan id: 1)
+                     Filter: ((a = a) AND (SubPlan 1))
+                     SubPlan 1  (slice4; segments: 3)
+                       ->  Materialize
+                             ->  Broadcast Motion 1:3  (slice2)
+                                   ->  Limit
+                                         ->  Gather Motion 3:1  (slice1; segments: 3)
+                                               ->  Seq Scan on non_part1
+               ->  Hash
+                     ->  Partition Selector for ds_main (dynamic scan id: 1)
+                           ->  Broadcast Motion 3:3  (slice3; segments: 3)
+                                 ->  Seq Scan on non_part2
+ Optimizer: Pivotal Optimizer (GPORCA)
+(19 rows)
+
+SELECT * FROM ds_main, non_part2 WHERE ds_main.c = non_part2.e AND a IN ( SELECT a FROM non_part1) order by a;
+ a | b | c  | e  | f  
+---+---+----+----+----
+ 1 | 2 |  3 |  3 |  4
+ 2 | 3 |  4 |  4 |  5
+ 3 | 4 |  5 |  5 |  6
+ 4 | 5 |  6 |  6 |  7
+ 5 | 6 |  7 |  7 |  8
+ 6 | 7 |  8 |  8 |  9
+ 7 | 8 |  9 |  9 | 10
+ 8 | 9 | 10 | 10 | 11
+(8 rows)
+
 -- As of this writing, pg_dump creates an invalid dump for some of the tables
 -- here. See https://github.com/greenplum-db/gpdb/issues/3598. So we must drop
 -- the tables, or the pg_upgrade test fails.

src/test/regress/sql/gporca.sql
@@ -2259,7 +2259,7 @@ analyze ds_part;
 analyze non_part1;
 analyze non_part2;
 SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
-explain SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
+explain analyze SELECT * FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b + 1 FROM non_part1);
 
 SELECT *, a IN ( SELECT b + 1 FROM non_part1) FROM ds_part, non_part2 WHERE ds_part.c = non_part2.e AND non_part2.f = 10 AND a IN ( SELECT b FROM non_part1);
 CREATE INDEX ds_idx ON ds_part(a);

src/test/regress/sql/qp_dropped_cols.sql
@@ -8624,6 +8624,36 @@ explain select * from multi_part_mid_bitmap where date = '2017-02-05' and region
 -- end_ignore
 select * from multi_part_mid_bitmap where date = '2017-02-05' and region = 'usa';
 
+-- Exchange partitions with dropped columns and check subplan with attribute remapping into dynamicSeqscan
+-- start_ignore
+DROP TABLE if exists ds_main;
+DROP TABLE if exists ds_part1;
+DROP TABLE if exists non_part1;
+DROP TABLE if exists non_part2;
+-- end_ignore
+
+CREATE TABLE ds_main ( a INT, b INT, c INT) PARTITION BY RANGE(c)( START(1) END (10) EVERY (2), DEFAULT PARTITION deflt);
+CREATE TABLE ds_part1 (a int, a1 int, b int, c int);
+CREATE TABLE non_part1 (c INT);
+CREATE TABLE non_part2 (e INT, f INT);
+
+SET optimizer_enforce_subplans TO ON;
+
+-- drop columns to get attribute remapping
+ALTER TABLE ds_part1 drop column a1;
+ALTER TABLE ds_main exchange partition for (1) with table ds_part1;
+
+INSERT INTO non_part1 SELECT i FROM generate_series(1, 1)i;
+INSERT INTO non_part2 SELECT i, i + 1 FROM generate_series(1, 10)i;
+INSERT INTO ds_main SELECT i, i + 1, i + 2 FROM generate_series (1, 1000)i;
+
+analyze ds_main;
+analyze non_part1;
+analyze non_part2;
+
+EXPLAIN (costs off) SELECT * FROM ds_main, non_part2 WHERE ds_main.c = non_part2.e AND a IN ( SELECT a FROM non_part1) order by a;
+SELECT * FROM ds_main, non_part2 WHERE ds_main.c = non_part2.e AND a IN ( SELECT a FROM non_part1) order by a;
+
 -- As of this writing, pg_dump creates an invalid dump for some of the tables
 -- here. See https://github.com/greenplum-db/gpdb/issues/3598. So we must drop
 -- the tables, or the pg_upgrade test fails.