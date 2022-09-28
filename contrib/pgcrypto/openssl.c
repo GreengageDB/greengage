a71ca4e4328c15c883ec7b8a1ebb12dc8d38fa98
Vasiliy Ivanov <ivi@arenadata.io>
2022-09-28 22:48:36 +0300

    Revert "remove include for fips header"
    To make changes in test files to sync 6.23
    
    This reverts commit 56d2c49960b73fc7bb6bb1078e746bdf93bdc561.

--- contrib/pgcrypto/openssl.c
+++ contrib/pgcrypto/openssl.c
@@ -40,6 +40,12 @@
 #include <openssl/rand.h>
 #include <openssl/err.h>
 
+#ifdef OPENSSL_FIPS
+#if __has_include("openssl/fips.h")
+#include <openssl/fips.h>
+#endif
+#endif
+
 #include "utils/memutils.h"
 #include "utils/resowner.h"
 
