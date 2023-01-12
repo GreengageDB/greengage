d2461dd67ba0ca5079a8466b9c5e9d70630c2439
Adam Lee <adlee@vmware.com>
2023-01-12 11:00:44 +0800

    ao/co: Decouple DML finish from finish_bulk_insert
    Since upstream commit f7c830f1ab2645236ac2d6103fb3a88518bdc4fc the                                                                                                                                                                     finish_bulk_insert table AM API can be called multiple times for the same table
    (for COPY). This leads to aoco_dml_finish and appendonly_dml_finish getting
    called multiple times for the same table, which is undesirable. We want the DML
    state to be set up and torn down once per COPY session for a given relation. So
    decouple the two.
    
    (cherry picked from commit e1b2ce7185191192e568f38f90388b611f7ac4e3)

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -6600,6 +6600,9 @@ ATRewriteTable(AlteredTableInfo *tab, Oid OIDNewHeap, LOCKMODE lockmode)
 
 		table_finish_bulk_insert(newrel, ti_options);
 
+		if (newrel->rd_tableam)
+			table_dml_finish(newrel);
+
 		table_close(newrel, NoLock);
 	}
 }
