9d88aae39c859392be2f6928ba82966b864ee39a
Vasiliy Ivanov <ivi@arenadata.io>
2023-02-09 16:43:00 +1000

    Revert "ORCA produces bogus plan for queries with CTE during handling distribution for Sequence children (#446)"
    to update mdid in this patch after applying
    a73405542cc409301439888e3b5cf54fbcad1b4f
    
    This reverts commit 8c79124bb6ff5465c220736ec0dbb4ad8be0ec4a.

--- src/backend/gporca/libgpopt/src/operators/CPhysicalSequence.cpp
+++ src/backend/gporca/libgpopt/src/operators/CPhysicalSequence.cpp
@@ -281,16 +281,6 @@ CPhysicalSequence::PdsRequired(CMemoryPool *mp,
 			true /* fAllowReplicated */, false /* fAllowEnforced */);
 	}
 
-	if ((CDistributionSpec::EdtSingleton == pdsRequired->Edt() &&
-		 CDistributionSpecSingleton::PdssConvert(pdsRequired)->FOnMaster()) ||
-		(CDistributionSpec::EdtNonSingleton == pdsRequired->Edt() &&
-		 CDistributionSpecNonSingleton::PdsConvert(pdsRequired)
-			 ->FProhibitReplicated()))
-	{
-		return GPOS_NEW(mp)
-			CDistributionSpecNonSingleton(true /* fProhibitReplicated */);
-	}
-
 	// first child is non-singleton, request a non-singleton distribution on second child
 	return GPOS_NEW(mp) CDistributionSpecNonSingleton();
 }
