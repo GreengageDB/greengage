5f91955e30390c319a970241f68107ebfd3b0792
Vasiliy Ivanov <ivi@arenadata.io>
2023-02-15 15:39:47 +1000

    change master to coorinator in internal pipelines
    To be consistent with inclusive terminology that was brought by
    fc99e830abe35d1dfc201388ff4f845aa2cadcf0

--- gpMgmt/test/behave_utils/arenadata/fixtures.py
+++ gpMgmt/test/behave_utils/arenadata/fixtures.py
@@ -7,5 +7,5 @@ def init_cluster(context):
     Given the database is not running
         And a working directory of the test as '/tmp/concourse_cluster'
         And the user runs command "rm -rf ~/gpAdminLogs/gpinitsystem*"
-        And a cluster is created with mirrors on "mdw" and "sdw1, sdw2, sdw3"
+        And a cluster is created with mirrors on "cdw" and "sdw1, sdw2, sdw3"
     """)
