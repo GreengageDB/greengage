c60885a204e6c5709c41a30b10ec9b62d8d6cc82
Huansong Fu <fuhuansong@gmail.com>
2023-02-26 21:24:56 -0800

    Fix inconsistency between gp_fastsequence row and index after crash
    When we insert into gp_fastsequence, we insert a frozen row
    (xmin=FrozenTransactionXid) and then update the index. That creates a
    possibility of inconsistency in presence of server crash: if the WAL
    for the frozen insert is flushed to persistent storage but the index
    is not, after crash recovery we would be able to see the frozen row
    in seqscan, but not in indexscan.
    
    Now fixing this problem by doing regular heap_insert (normal MVCC) and
    update the index first. Only after that we mark the row to be frozen.
    That way we remove the inconsistency: if we crashed before we mark the
    row frozen, the row follows regular MVCC and we won't be able to see it;
    or if we crashed after we marked it frozen (and also it made to the disk),
    we would replay both table row and index, so we would see the row in both
    seqscan and indexscan.
    
    Note that, now we use the upstream-aligned way to mark the tuple frozen
    which is infomask |= HEAP_XMIN_FROZEN (commit 37484ad2aace), instead of
    setting xmin = FrozenTransactionId. The freeze effect should be the same
    though, but it helps leave behind a trail as to which transaction did the
    freezing.
    
    In addition, we use the same WAL type XLOG_HEAP2_FREEZE_PAGE as vacuum,
    so we also align with upstream in how to replay the frozen tuple.
    The alternative was to use the WAL type for heap_inplace_update, but then
    we would need to change it to update the header portion as well as data
    portion, which would be an unpleasant difference in WAL semantics.
    
    The only other thing that has been changed from the previous way of doing
    frozen_heap_insert is that now we don't set XLogRecord.xl_xid to
    FrozenTransactionId anymore. However, this should be OK because:
    (1) we are not setting xim=FrozenTransactionId anymore, the WAL record
    should remain consistent.
    (2) the change where we were setting this field in Greenplum seems to be
    outdated itself. Upstream doesn't have even the capability to do that: it
    never passes an arbitrary XID to that field, it's always
    GetCurrentTransactionIdIfAny() (see XLogRecordAssemble()), even if you
    insert a tuple w/ xmin=FrozenTransactionId (e.g. for PG sequence, see
    fill_seq_with_data()). GPDB doesn't have a reason to differ.
    
    Finally, need to make a fewn adjustment in the regress test output where
    xmin is queried for gp_fastsequence.
    
    This is backport of 961de2d
    
    Co-authored-by: Ashwin Agrawal <aashwin@vmware.com>

--- src/backend/catalog/gp_fastsequence.c
+++ src/backend/catalog/gp_fastsequence.c
@@ -21,6 +21,8 @@
 #include "access/genam.h"
 #include "access/htup.h"
 #include "access/heapam.h"
+#include "access/xact.h"
+#include "utils/faultinjector.h"
 #include "utils/syscache.h"
 #include "utils/tqual.h"
 #include "access/appendonlywriter.h"
@@ -179,7 +181,27 @@ insert_or_update_fastsequence(Relation gp_fastsequence_rel,
 
 		newTuple = heaptuple_form_to(tupleDesc, values, nulls, NULL, NULL);
 
-		CatalogTupleInsertFrozen(gp_fastsequence_rel, newTuple);
+		/* insert the tuple */
+		CatalogTupleInsert(gp_fastsequence_rel, newTuple);
+
+#ifdef FAULT_INJECTOR
+		FaultInjector_InjectFaultIfSet(
+								"insert_fastsequence_before_freeze",
+								DDLNotSpecified,
+								"", //databaseName
+								RelationGetRelationName(gp_fastsequence_rel));
+#endif
+
+		/* freeze the tuple */
+		heap_freeze_tuple_wal_logged(gp_fastsequence_rel, newTuple);
+
+#ifdef FAULT_INJECTOR
+		FaultInjector_InjectFaultIfSet(
+								"insert_fastsequence_after_freeze",
+								DDLNotSpecified,
+								"", //databaseName
+								RelationGetRelationName(gp_fastsequence_rel));
+#endif
 
 		heap_freetuple(newTuple);
 	}
