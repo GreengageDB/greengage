3eeda2a8a18a4134d53135c262336c4a0f41dbb6
Evgeniy Ratkov <e.ratkov@arenadata.io>
2023-06-13 10:21:44 +0300

    arenadata_toolkit: add new views db_files_current, db_segment_files (#537)
    This patch extends initial version of arenadata_toolkit extension.
    New views were added to arenadata_toolkit--1.0.sql script:
    - arenadata_toolkit.__db_segment_files
    - arenadata_toolkit.__db_files_current
    - arenadata_toolkit.__db_files_current_unmapped
    New function was added to arenadata_toolkit.c:
    - adb_get_relfilenodes
    
    These views return list of files of database's filesystem with params.

--- gpcontrib/arenadata_toolkit/arenadata_toolkit--1.0.sql
+++ gpcontrib/arenadata_toolkit/arenadata_toolkit--1.0.sql
@@ -197,3 +197,201 @@ EXECUTE ON MASTER;
  Only for admin usage.
  */
 REVOKE ALL ON FUNCTION arenadata_toolkit.adb_create_tables() FROM public;
+
+CREATE FUNCTION arenadata_toolkit.adb_get_relfilenodes(tablespace_oid OID)
+RETURNS TABLE (
+	segindex INT2,
+	dbid INT2,
+	datoid OID,
+	tablespace_oid OID,
+	relfilepath TEXT,
+	relfilenode OID,
+	reloid OID,
+	size BIGINT,
+	modified_dttm TIMESTAMP WITHOUT TIME ZONE,
+	changed_dttm TIMESTAMP WITHOUT TIME ZONE
+)
+EXECUTE ON ALL SEGMENTS
+AS '$libdir/arenadata_toolkit', 'adb_get_relfilenodes'
+LANGUAGE C STABLE STRICT;
+
+GRANT EXECUTE ON FUNCTION arenadata_toolkit.adb_get_relfilenodes(OID)
+	TO public;
+
+CREATE VIEW arenadata_toolkit.__db_segment_files
+AS
+SELECT
+	segfiles.segindex,
+	segfiles.dbid,
+	segfiles.datoid,
+	segfiles.tablespace_oid,
+	gpconf.datadir || '/' || relfilepath AS full_path,
+	segfiles.size,
+	segfiles.relfilenode,
+	gpconf.preferred_role AS segment_preferred_role,
+	gpconf.hostname AS hostname,
+	gpconf.address AS address,
+	segfiles.reloid,
+	segfiles.modified_dttm,
+	segfiles.changed_dttm
+FROM pg_tablespace tbl, arenadata_toolkit.adb_get_relfilenodes(tbl.oid) AS segfiles
+		 INNER JOIN gp_segment_configuration AS gpconf
+					ON segfiles.dbid = gpconf.dbid;
+
+GRANT SELECT ON arenadata_toolkit.__db_segment_files TO public;
+
+CREATE VIEW arenadata_toolkit.__db_files_current
+AS
+SELECT
+	c.oid AS oid,
+	c.relname AS table_name,
+	n.nspname AS table_schema,
+	c.relkind AS type,
+	c.relstorage AS storage,
+	d.datname AS table_database,
+	t.spcname AS table_tablespace,
+	dbf.segindex AS content,
+	dbf.segment_preferred_role AS segment_preferred_role,
+	dbf.hostname AS hostname,
+	dbf.address AS address,
+	dbf.full_path AS file,
+	dbf.size AS file_size,
+	dbf.modified_dttm AS modifiedtime,
+	dbf.changed_dttm AS changedtime
+FROM arenadata_toolkit.__db_segment_files dbf
+		 LEFT JOIN pg_class AS c
+				   ON c.oid = dbf.reloid
+		 LEFT JOIN pg_namespace n
+				   ON c.relnamespace = n.oid
+		 LEFT JOIN pg_tablespace t
+				   ON dbf.tablespace_oid = t.oid
+		 LEFT JOIN pg_database d
+				   ON dbf.datoid = d.oid;
+
+GRANT SELECT ON arenadata_toolkit.__db_files_current TO public;
+
+CREATE VIEW arenadata_toolkit.__db_files_current_unmapped
+AS
+SELECT
+	v.table_database,
+	v.table_tablespace,
+	v.content,
+	v.segment_preferred_role,
+	v.hostname,
+	v.address,
+	v.file,
+	v.file_size
+FROM arenadata_toolkit.__db_files_current v
+WHERE v.oid IS NULL;
+
+GRANT SELECT ON arenadata_toolkit.__db_files_current_unmapped TO public;
+
+/*
+ This is part of arenadata_toolkit API for ADB Bundle.
+ This function collects information in db_files_current and db_files_history tables.
+ */
+CREATE FUNCTION arenadata_toolkit.adb_collect_table_stats()
+RETURNS VOID
+AS $$
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_partitions p
+					WHERE
+					p.schemaname = 'arenadata_toolkit'
+					AND
+					p.partitionschemaname = 'arenadata_toolkit'
+					AND
+					p.tablename = 'db_files_history'
+					AND
+					now() BETWEEN CAST(substring(partitionrangestart FROM '#"%#"::%' FOR '#') AS TIMESTAMP WITHOUT TIME ZONE)
+					AND
+					CAST(substring(partitionrangeend FROM '#"%#"::%' FOR '#') AS TIMESTAMP WITHOUT TIME ZONE)
+					AND
+					p.partitiontype = 'range'
+					AND
+					partitionstartinclusive = 't')
+	THEN
+		EXECUTE FORMAT($fmt$ALTER TABLE arenadata_toolkit.db_files_history SPLIT DEFAULT PARTITION
+			START (date %1$L) INCLUSIVE
+			END (date %2$L) EXCLUSIVE
+			INTO (PARTITION %3$I, default partition);$fmt$,
+				to_char(now(), 'YYYY-MM-01'),
+				to_char(now() + interval '1 month','YYYY-MM-01'),
+				'p'||to_char(now(), 'YYYYMM'));
+	END IF;
+
+	CREATE TEMPORARY TABLE IF NOT EXISTS db_files_current
+	(
+		oid BIGINT,
+		table_name TEXT,
+		table_schema TEXT,
+		type CHAR(1),
+		storage CHAR(1),
+		table_parent_table TEXT,
+		table_parent_schema TEXT,
+		table_database TEXT,
+		table_tablespace TEXT,
+		"content" INTEGER,
+		segment_preferred_role CHAR(1),
+		hostname TEXT,
+		address TEXT,
+		file TEXT,
+		modifiedtime TIMESTAMP WITHOUT TIME ZONE,
+		file_size BIGINT
+	)
+	DISTRIBUTED RANDOMLY;
+
+	/*
+		Since this table is temporary and user can call this several times in a session
+		then we need to truncate it (an alternative will be to drop it).
+	 */
+	TRUNCATE TABLE db_files_current;
+
+	/* We use temporary table in this case, because we don't want to add own own unmapped
+	   relfilenode oids in a db_files_current and db_files_history tables. The downside of
+	   this approach that if time between calls to adb_collect_table_stats will be less
+	   then checkpointing interval (there will be no unlink peformed), than these unmapped
+	   files will still go to db_files_current and db_files_history as unmapped.
+	*/
+	INSERT INTO db_files_current
+	SELECT
+		f.oid,
+		f.table_name,
+		f.table_schema,
+		f.type,
+		f.storage,
+		part.tablename AS table_parent_table,
+		part.schemaname AS table_parent_schema,
+		f.table_database,
+		f.table_tablespace,
+		f."content",
+		f.segment_preferred_role,
+		f.hostname,
+		f.address,
+		f.file,
+		CURRENT_TIMESTAMP AS modifiedtime,
+		f.file_size
+	FROM arenadata_toolkit.__db_files_current AS f
+	LEFT JOIN pg_partitions AS part
+		ON (part.partitionschemaname = f.table_schema AND part.partitiontablename = f.table_name);
+
+	/*
+	 Here we may truncate it, it's relfilenodes aren't in db_files_current temporary table.
+	 */
+	TRUNCATE TABLE arenadata_toolkit.db_files_current;
+
+	INSERT INTO arenadata_toolkit.db_files_current
+	SELECT *
+	FROM db_files_current;
+
+	INSERT INTO arenadata_toolkit.db_files_history
+	SELECT *, now() AS collecttime
+	FROM arenadata_toolkit.db_files_current;
+
+END$$
+LANGUAGE plpgsql VOLATILE
+EXECUTE ON MASTER;
+
+/*
+	Only for admin usage.
+ */
+REVOKE ALL ON FUNCTION arenadata_toolkit.adb_collect_table_stats() FROM public;
--- gpcontrib/arenadata_toolkit/arenadata_toolkit.c
+++ gpcontrib/arenadata_toolkit/arenadata_toolkit.c
@@ -1,4 +1,7 @@
+#include <dirent.h>
 #include <sys/stat.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #include "postgres.h"
 
@@ -7,12 +10,19 @@
 #include "cdb/cdbvars.h"
 #include "common/relpath.h"
 #include "fmgr.h"
+#include "funcapi.h"
 #include "miscadmin.h"
-#include "storage/lock.h"
+#include "storage/fd.h"
+#include "nodes/execnodes.h"
+#include "cdb/cdbvars.h"
 #include "utils/builtins.h"
+#include "utils/relfilenodemap.h"
+#include "utils/timestamp.h"
 #include "utils/elog.h"
 #include "utils/rel.h"
 #include "utils/relcache.h"
+#include "catalog/pg_tablespace.h"
+#include "storage/lock.h"
 
 PG_MODULE_MAGIC;
 
@@ -196,3 +206,140 @@ get_ao_storage_total_bytes(Relation rel, char *relpath)
 	ao_foreach_extent_file(calculate_ao_storage_perSegFile, &ctx);
 	return ctx.total_size;
 }
+
+typedef struct
+{
+	char     *datpath;
+	DIR      *dirdesc;
+	TupleDesc tupdesc;
+} user_fctx_data;
+
+/*
+ * Name of file must be "XXX.X" or "XXX"
+ * where XXX is Oid. OID must be not more than OID_MAX.
+ */
+static Oid get_oid_from_filename(const char *filename)
+{
+	unsigned long int oid, segment;
+	char trailer;
+
+	int count = sscanf(filename, "%lu.%lu%c", &oid, &segment, &trailer);
+	if (count < 1 || count > 2)
+		return InvalidOid;
+	if (oid > OID_MAX)
+		return InvalidOid;
+
+	return (Oid) oid;
+}
+
+PG_FUNCTION_INFO_V1(adb_get_relfilenodes);
+Datum adb_get_relfilenodes(PG_FUNCTION_ARGS)
+{
+	Oid              datoid = MyDatabaseId;
+	Oid              tablespace_oid = PG_GETARG_OID(0);
+
+	struct dirent   *direntry;
+	user_fctx_data  *fctx_data;
+	FuncCallContext *funcctx;
+
+	if (tablespace_oid == GLOBALTABLESPACE_OID)
+		datoid = 0;
+
+	if (SRF_IS_FIRSTCALL())
+	{
+		MemoryContext oldcontext;
+
+		funcctx = SRF_FIRSTCALL_INIT();
+		oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+		fctx_data = palloc(sizeof(user_fctx_data));
+		fctx_data->datpath = GetDatabasePath(datoid, tablespace_oid);
+		fctx_data->dirdesc = AllocateDir(fctx_data->datpath);
+
+		if (!fctx_data->dirdesc)
+		{
+			/* Nothing to do: empty tablespace (maybe it has been just created)*/
+			MemoryContextSwitchTo(oldcontext);
+			SRF_RETURN_DONE(funcctx);
+		}
+
+		if (get_call_result_type(fcinfo, NULL, &fctx_data->tupdesc)
+				!= TYPEFUNC_COMPOSITE)
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					 errmsg("return type must be a row type")));
+
+		funcctx->attinmeta = TupleDescGetAttInMetadata(fctx_data->tupdesc);
+		funcctx->user_fctx = fctx_data;
+
+		MemoryContextSwitchTo(oldcontext);
+	}
+
+	funcctx = SRF_PERCALL_SETUP();
+	fctx_data = (user_fctx_data *) funcctx->user_fctx;
+
+	while ((direntry = ReadDir(fctx_data->dirdesc, fctx_data->datpath)) != NULL)
+	{
+		struct stat fst;
+		Datum       values[10];
+		bool        nulls[10];
+		char       *filename;
+		Oid         reloid;
+		Oid         relfilenode_oid;
+		HeapTuple   tuple;
+
+		CHECK_FOR_INTERRUPTS();
+
+		if (direntry->d_type == DT_DIR)
+			continue;
+
+		relfilenode_oid = get_oid_from_filename(direntry->d_name);
+		if (relfilenode_oid == InvalidOid)
+			continue;
+
+		filename = psprintf("%s/%s", fctx_data->datpath, direntry->d_name);
+
+		if (stat(filename, &fst) < 0)
+		{
+			if (errno == ENOENT)
+				continue;
+
+			ereport(ERROR,
+					(errcode_for_file_access(),
+					 errmsg("could not stat file \"%s\": %m",
+							filename)));
+		}
+
+		memset(values, 0, sizeof(values));
+		memset(nulls, 0, sizeof(nulls));
+
+		values[0] = Int16GetDatum(GpIdentity.segindex);
+		values[1] = Int16GetDatum(GpIdentity.dbid);
+		values[2] = DatumGetObjectId(datoid);
+		values[3] = DatumGetObjectId(tablespace_oid);
+		values[4] = CStringGetTextDatum(filename);
+
+		reloid = RelidByRelfilenode(tablespace_oid, relfilenode_oid);
+		if (OidIsValid(reloid))
+		{
+			values[5] = DatumGetObjectId(relfilenode_oid);
+			values[6] = DatumGetObjectId(reloid);
+		}
+		else
+		{
+			nulls[5] = true;
+			nulls[6] = true;
+		}
+
+		values[7] = Int64GetDatum(fst.st_size);
+		values[8] = TimestampGetDatum(time_t_to_timestamptz(fst.st_mtime));
+		values[9] = TimestampGetDatum(time_t_to_timestamptz(fst.st_ctime));
+
+		tuple = heap_form_tuple(fctx_data->tupdesc, values, nulls);
+		SRF_RETURN_NEXT(funcctx, HeapTupleGetDatum(tuple));
+	}
+
+	FreeDir(fctx_data->dirdesc);
+	SRF_RETURN_DONE(funcctx);
+}
+
