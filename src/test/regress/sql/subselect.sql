ca89de312ed32d602ebec69d3f84196ddf437a77
Alexander Kondakov <a.kondakov@arenadata.io>
2023-08-22 08:53:16 +0300

    Fix ORCA invalid processing of nested SubLinks with GROUP BY attributes
    The ORCA optimizer could produce an invalid query plan due to incorrect
    processing of nested SubLinks (SubLink contains one more SubLink or rtable
    subquery inside) during query normalization.
    
    The incorrect behaviour took place when a query had a nested
    SubLink in its targetList, and SubLink lower levels contained attributes that
    were referenced in the query's GROUP BY clause. ORCA could construct for
    such queries an invalid plan, according to which some wrong and unexpected
    attributes got into the nested part of SubLink's plan (the example is provided
    in tests section). And the presence of those attributes could lead to incorrect
    query execution or even segmentation fault.
    
    Inside the NormalizeGroupByProjList function the context.m_lower_table_tlist,
    which contains grouping attributes from target_list_copy, is created. And the
    entries of target_list_copy, that are not grouping attributes, are mutated
    inside the RunExtractAggregatesMutator in order to make their inner expressions
    reference the corresponding entries from context.m_lower_table_tlist.
    The invalid plan could be produced because of incorrect mapping between Var's
    varattno (in target_list_copy) and some TargetEntry's resno
    (in context.m_lower_table_tlist). This mapping is established by
    RunExtractAggregatesMutator, where for the Var code branch the following
    assignment takes place: var->varattno = found_tle->resno, where found_tle is the
    corresponding entry from context.m_lower_table_tlist. However, the behaviour of
    this function was erroneous for the case of nested SubLinks because it
    mutated only the upper level of nested SubLink structure, and other subqueries
    remained unmodified because RunExtractAggregatesMutator did not have a branch
    for the Query, which was required for futher diving into lower subqueries. And
    because inner Vars hadn't been mutated, they started reference wrong attributes.
    
    This patch modifies RunExtractAggregatesMutator function by adding the Query
    branch to make the mutator correctly step into nested SubLink queries. And the
    call of MutateQueryOrExpressionTree is replaced with the call of mutator itself
    in order to correctly modify m_current_query_level when stepping into lower
    subqueries.

--- src/test/regress/sql/subselect.sql
+++ src/test/regress/sql/subselect.sql
@@ -517,3 +517,25 @@ fetch backward all in c1;
 
 commit;
 --end_ignore
+
+-- Ensure that both planners produce valid plans for the query with the nested
+-- SubLink, which contains attributes referenced in query's GROUP BY clause.
+-- The inner part of SubPlan should contain only t.j.
+-- start_ignore
+drop table if exists t;
+-- end_ignore
+create table t (i int, j int) distributed by (i);
+insert into t values (1, 2);
+
+explain (verbose, costs off)
+select j,
+(select j from (select j) q2)
+from t
+group by i, j;
+
+select j,
+(select j from (select j) q2)
+from t
+group by i, j;
+
+drop table t;
