9c7809193b0a9c10d38ab3bf21f2073210179e26
Vladimir Sarmin <v.sarmin@arenadata.io>
2024-02-12 11:05:10 +0300

    Move for loop under a null check (#739)
    Move for loop that is supposed to free gpsets from canonical_grpsets under a
    null check to avoid dereferencing a NULL pointer, if canonical_grpsets was
    passed as NULL to free_canonical_groupingsets().

--- src/backend/optimizer/plan/plangroupext.c
+++ src/backend/optimizer/plan/plangroupext.c
@@ -539,14 +539,11 @@ plan_grouping_extension(PlannerInfo *root,
 void
 free_canonical_groupingsets(CanonicalGroupingSets *canonical_grpsets)
 {
-	int grpset_no;
-	for (grpset_no=0; grpset_no<canonical_grpsets->ngrpsets; grpset_no++)
-	{
-		bms_free(canonical_grpsets->grpsets[grpset_no]);
-	}
-	
 	if (canonical_grpsets)
 	{
+		for (int grpset_no=0; grpset_no<canonical_grpsets->ngrpsets; grpset_no++)
+			bms_free(canonical_grpsets->grpsets[grpset_no]);
+
 		if (canonical_grpsets->grpsets)
 			pfree(canonical_grpsets->grpsets);
 		if (canonical_grpsets->grpset_counts)
