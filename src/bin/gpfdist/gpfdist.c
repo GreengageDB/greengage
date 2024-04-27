d21d55d2929b6757a4ddc43379ae3a5729d45bd2
Alexey Gordeev <goa@arenadata.io>
2024-04-27 18:42:05 +0500

    Implement default transformation flags for gpfdist (#934)
    Currently, gpfdist transformation can only be applied to table source on
    external table level. It can be done by specifying #transform tag. Sometimes it
    may be not handy to specify the same transformation for all source files. The
    goal of the patch is to implement new flags with the default transformation name
    for input and output transformations. The default transformation(s) can be set
    at gpfdist start and applied only if table level transformation is not
    specified.
    
    New flags are a subject of only basic check. The idea was to keep the same
    behavior and check transformation existence only at runtime.
    The test shows a behavior described in patch goal.
    
    (cherry picked from commit 2f6e3ef9af0c74b5297a5577566fbf0d6b8c58e4)

--- src/bin/gpfdist/gpfdist.c
+++ src/bin/gpfdist/gpfdist.c
@@ -203,7 +203,9 @@ static struct
 	int 		k; /* The time used to clean up sessions in seconds */
 	int			compress; /* The flag to indicate whether comopression transmission is open */
 	int			multi_thread; /* The number of working threads for compression transmission */
-} opt = { 8080, 8080, 0, 0, 0, ".", 0, 0, -1, 5, 0, 32768, 0, 256, 0, 0, 0, 0, 300, 0, 0};
+	const char* I; /* default input transformation */
+	const char* O; /* default output transformation */
+} opt = { 8080, 8080, 0, 0, 0, ".", 0, 0, -1, 5, 0, 32768, 0, 256, 0, 0, 0, 0, 300, 0, 0, 0, 0};
 
 #define START_BUFFER_SIZE (1 << 20) /* 1M as start size */
 #define MAXIMUM_BUFFER_SIZE (1 << 30) /* 1G as Maximum size */
@@ -622,6 +624,8 @@ static void usage_error(const char* msg, int print_usage)
 #endif
 #ifdef GPFXDIST
 					    "        -c file    : configuration file for transformations\n"
+						"        -I name    : name of default input transformation\n"
+						"        -O name    : name of default output transformation\n"
 #endif
 #ifdef HAVE_LIBZSTD
 						"        --compress : open compression transmission\n"
@@ -689,6 +693,8 @@ static void parse_command_line(int argc, const char* const argv[],
 	{ "ssl", 257, 1, "ssl - certificates files under this directory" },
 #ifdef GPFXDIST
 	{ NULL, 'c', 1, "transform configuration file" },
+	{ NULL, 'I', 1, "default input transformation" },
+	{ NULL, 'O', 1, "default output transformation" },
 #endif
 	{ "version", 256, 0, "print version number" },
 	{ NULL, 'w', 1, "wait for session timeout in seconds" },
@@ -805,6 +811,12 @@ static void parse_command_line(int argc, const char* const argv[],
 			usage_error("Multi-thread transmission relies on zstd, but zstd is not supported by this build", 0);
 			break;
 #endif
+		case 'I':
+			opt.I = arg;
+			break;
+		case 'O':
+			opt.O = arg;
+			break;
 		}
 	}
 	if (e != APR_EOF)
@@ -967,6 +979,37 @@ static void parse_command_line(int argc, const char* const argv[],
 			exit(1);
         }
     }
+
+	/* validate opt.I and opt.O */
+	if (opt.I || opt.O)
+	{
+		extern const char* validate_transform_opt(struct transform* trlist, const char* name);
+
+		char *p;
+		const char *err;
+
+		if (opt.I)
+		{
+			p = gstring_trim(apr_pstrdup(pool, opt.I));
+			err = validate_transform_opt(opt.trlist, p);
+
+			if (err)
+				usage_error(err, 0);
+
+			opt.I = p;
+		}
+
+		if (opt.O)
+		{
+			p = gstring_trim(apr_pstrdup(pool, opt.O));
+			err = validate_transform_opt(opt.trlist, p);
+
+			if (err)
+				usage_error(err, 0);
+
+			opt.O = p;
+		}
+	}
 #endif
 
 	/* there should not be any more args left */
@@ -3986,6 +4029,14 @@ static int request_set_transform(request_t *r)
 		*start = 0;
 		if (! r->trans.name)
 			r->trans.name = start + strlen(param);
+	}
+
+	if (! r->trans.name)
+	{
+		if (r->is_get)
+			r->trans.name = opt.I;
+		else
+			r->trans.name = opt.O;
 	}
 
 	if (! r->trans.name)
