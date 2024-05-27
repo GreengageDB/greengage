c3e4bf3170af2a715485ec4c2c46d4760b6793e9
Evgeniy Ratkov <e.ratkov@arenadata.io>
2024-05-27 13:39:25 +0300

    Revert "Avoid linking AO aux tables to extension (#930)"
    to solve conflicts with 482d4e839d519dcfde9cddf03807787d9d91d015
    
    This reverts commit 10ec9c7ae00475f0db823d53a038b3768b679d63.

--- src/backend/catalog/heap.c
+++ src/backend/catalog/heap.c
@@ -1725,12 +1725,9 @@ heap_create_with_catalog(const char *relname,
 	 *
 	 * Also, skip this in bootstrap mode, since we don't make dependencies
 	 * while bootstrapping.
-	 *
-	 * GPDB: The section about TOAST is still relevant for AO aux tables.
 	 */
 	if (relkind != RELKIND_COMPOSITE_TYPE &&
 		relkind != RELKIND_TOASTVALUE &&
-		!IsAppendonlyMetadataRelkind(relkind) &&
 		!IsBootstrapProcessingMode())
 	{
 		ObjectAddress myself,
--- src/test/modules/test_extensions/test_ext_ao--1.0.sql
+++
@@ -1,6 +0,0 @@
-/* src/test/modules/test_extensions/test_ext_ao--1.0.sql */
-
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION test_ext_ao" to load this file. \quit
-
-create table test_ext_ao_table(i int) with (appendonly=true);
