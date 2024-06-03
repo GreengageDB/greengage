jnihal/gpdb

gpexpand: skip expanded/broken tables when redistributing
This commit is a cherry-pick of the community PR #15961.
In summary `gpexpand redistribution` earlier used to error out when the table would have been already expanded manually by the user or the table would have been damaged. This commit changes this behaviour so that any expanded/broken tables are skipped without affecting the redistribution for other tables.

Co-authored-by: Dmitry Smal <mialinx@yandex-team.ru>
Co-authored-by: Nihal Jain <jnihal@vmware.com>

gpMgmt/test/behave/mgmt_utils/steps/mgmt_utils.py
@@ -2927,6 +2927,7 @@ def _gpexpand_redistribute(context, duration=False, endtime=False):
     gpexpand = Gpexpand(context, working_directory=context.working_directory)
     context.command = gpexpand
     ret_code, std_err, std_out = gpexpand.redistribute(duration, endtime)
+    context.stdout_message = std_out
     if duration or endtime:
         if ret_code != 0:
             # gpexpand exited on time, it's expected
@@ -3257,6 +3258,23 @@ def impl(context):
             raise Exception("table %s has not finished expanding" % row[0])
     finally:
         conn.close()
+
+
+@then('table "{table_name}" {expansion_state} be marked as expanded')
+def impl(context, table_name, expansion_state):
+    dbname = 'postgres'
+    conn = dbconn.connect(dbconn.DbURL(dbname=dbname), unsetSearchPath=False)
+    try:
+        query = """select status from gpexpand.status_detail where fq_name='public.{0}'""".format(table_name)
+        result = dbconn.querySingleton(conn, query)
+
+        if result == 'NOT STARTED' and expansion_state == "should":
+            raise Exception("table {0} is not marked as expanded".format(table_name))
+        if result == "COMPLETED" and expansion_state == "should not":
+            raise Exception("table {0} is marked as expanded".format(table_name))
+    finally:
+        conn.close()
+
 
 @given('an FTS probe is triggered')
 @when('an FTS probe is triggered')

GreengageDB/greengage

This commit is a cherry-pick of 6b7e4c3.
In summary `gpexpand redistribution` earlier used to error out when the table
would have been already expanded manually by the user or the table would have
been damaged. This commit changes this behaviour so that any expanded/broken
tables are skipped without affecting the redistribution for other tables.
Error message was changed due two difference in messages of 6x and 7x.
execSingleton was changed to execSQLForSingleton in mgmt_utils.py to make it
work on 6x. Also, gpexpand.feature test was brought to a common standard by
adding 'rm -rf ~/gpAdminLogs/gpinitsystem*'. Without this line a failing
test would cause other tests to fail.
(cherry picked from commit cfb97d174c3e3b7987771a5c95b750f6a8c7febb)

gpMgmt/test/behave/mgmt_utils/steps/mgmt_utils.py
@@ -3143,6 +3143,7 @@ def _gpexpand_redistribute(context, duration=False, endtime=False):
     gpexpand = Gpexpand(context, working_directory=context.working_directory)
     context.command = gpexpand
     ret_code, std_err, std_out = gpexpand.redistribute(duration, endtime)
+    context.stdout_message = std_out
     if duration or endtime:
         if ret_code != 0:
             # gpexpand exited on time, it's expected
@@ -3457,6 +3458,23 @@ def impl(context):
         row = cursor.fetchone()
         if row:
             raise Exception("table %s has not finished expanding" % row[0])
+
+
+@then('table "{table_name}" {expansion_state} be marked as expanded')
+def impl(context, table_name, expansion_state):
+    dbname = 'postgres'
+    conn = dbconn.connect(dbconn.DbURL(dbname=dbname), unsetSearchPath=False)
+    try:
+        query = """select status from gpexpand.status_detail where fq_name='public.{0}'""".format(table_name)
+        result = dbconn.execSQLForSingleton(conn, query)
+
+        if result == 'NOT STARTED' and expansion_state == "should":
+            raise Exception("table {0} is not marked as expanded".format(table_name))
+        if result == "COMPLETED" and expansion_state == "should not":
+            raise Exception("table {0} is marked as expanded".format(table_name))
+    finally:
+        conn.close()
+
 
 @given('an FTS probe is triggered')
 @when('an FTS probe is triggered')