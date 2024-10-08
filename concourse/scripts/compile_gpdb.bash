8add3dd12c83c79ceb2496f3aec1dee6dc440cda
Vasiliy Ivanov <ivi@arenadata.io>
2024-10-08 15:57:14 +0200

    Revert "Add plpython3u binaries for gpdb 6 regress image (#1019)"
    to replace the similar commit from develop branch
    
    This reverts commit ca1ebaad090bb7e6ea4769553dcba35463f4a07c.

--- concourse/scripts/compile_gpdb.bash
+++ concourse/scripts/compile_gpdb.bash
@@ -59,15 +59,6 @@ function build_gpdb() {
 		make GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s dist
 	fi
 	popd
-
-	if [ -n "${PYTHON3}" ]; then
-		export PYTHON=${PYTHON3}
-		make -C ${GPDB_SRC_PATH}/src/pl/plpython clean
-		pushd ${GPDB_SRC_PATH}
-		./config.status --recheck
-		make -C src/pl/plpython install
-		popd
-	fi
 }
 
 function git_info() {
