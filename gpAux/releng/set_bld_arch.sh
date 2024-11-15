d995de56ad7599ca23bd08c7c5500ac1278fb310
Artem Eluferev <ae@arenadata.io>
2024-11-15 10:36:08 +0300

    ADBDEV-5732 add build support for Redos 7.3
    Updadate Makefiles and scripts to add build support for Redos 7.3

--- gpAux/releng/set_bld_arch.sh
+++ gpAux/releng/set_bld_arch.sh
@@ -4,7 +4,7 @@
 
 case "`uname -s`" in
     Linux)
-    if [ -f /etc/redhat-release -a ! -f /etc/altlinux-release ]; then
+    if [ -f /etc/redhat-release -a ! -f /etc/altlinux-release -a ! -f /etc/redos-release ]; then
         case "`cat /etc/redhat-release`" in
             *)
             BLD_ARCH_HOST="rhel`cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}'`_`uname -m | sed -e s/i686/x86_32/`"
