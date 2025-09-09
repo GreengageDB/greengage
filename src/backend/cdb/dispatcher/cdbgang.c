707a6fde00a1b8dcf61f8ca0c9b11c0887c0ed02
Vladimir Sarmin <v.sarmin@arenadata.io>
2025-09-09 18:03:25 +0300

    reset latestCursorCommandId on reset session to avoid collision with gp_command_count

--- src/backend/cdb/dispatcher/cdbgang.c
+++ src/backend/cdb/dispatcher/cdbgang.c
@@ -25,6 +25,7 @@
 #include "postmaster/postmaster.h"
 #include "tcop/tcopprot.h"
 #include "utils/int8.h"
+#include "utils/session_state.h"
 #include "utils/sharedsnapshot.h"
 #include "tcop/pquery.h"
 
@@ -812,6 +813,7 @@ GpResetSessionIfNeeded(void)
 		gp_session_id = newSessionId;
 		gp_command_count = 0;
 		MyProc->queryCommandId = 0;
+		MySessionState->latestCursorCommandId = 0;
 		pgstat_report_sessionid(newSessionId);
 
 		/* Update the slotid for our singleton reader. */
