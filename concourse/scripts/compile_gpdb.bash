cb53fa1a74fc9d716d95b0221c730bca27fd3da9
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2025-10-06 14:41:53 +0300

    Revert "Fix buuild compile_gpdb script"
    This reverts commit 057061d85950d28070bd955c0145fd1c5b45ade8.

--- concourse/scripts/compile_gpdb.bash
+++ concourse/scripts/compile_gpdb.bash
@@ -54,9 +54,9 @@ function link_python() {
 function build_gpdb() {
 	pushd ${GPDB_SRC_PATH}/gpAux
 	if [ -n "$1" ]; then
-		make "$1" GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s dist
+		make "$1" GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s devel
 	else
-		make GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s dist
+		make GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s devel
 	fi
 	popd
 
