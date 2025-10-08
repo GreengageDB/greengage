2fa224520f80aac94b8d266f5b929e0768fe8537
Alexander Kondakov <a.kondakov@arenadata.io>
2025-10-08 18:45:20 +0300

    Accept vacuum operations in XactLockTableWait assertion
    Function XactLockTableWait() calls LocalXidGetDistributedXid() which
    may get gxid corresponding to local wait xid from distributed clog in
    case if the dtx (which we are waiting for) managed to commit by that time we
    access its gxid. And for such case there is an assertion introduced by
    commit 13a1f66. The assert indicates that the commited transaction was
    just running in parallel with current one, meaning there is no other
    reason to access distributed transaction history. If the transaction was
    commited long time ago the XactLockTableWait() would never be called.
    
    However, there is a case when we can't compare the timestamps:
    vacuum operation, which performs in-place update of pg_database
    (or pg_class) without being in distributed transaction. For this
    case this patch extends the assertion by allowing current timestamp to
    have zero value.
    
    The new test related to this case is added to file 2c2753a.

--- src/test/isolation2/sql/intra-grant-inplace-db.sql
+++ src/test/isolation2/sql/intra-grant-inplace-db.sql
@@ -31,3 +31,37 @@ DROP ROLE regress_temp_grantee;
 1q:
 2q:
 3q:
+
+-- Same test as the above, except the GRANT transaction commits before the
+-- second transaction check the wait gxid, it should get the gxid from
+-- pg_distributedlog instead of the procarray.
+CREATE ROLE regress_temp_grantee;
+
+3: CREATE TEMPORARY TABLE frozen_witness (x xid) distributed by (x);
+-- observe datfrozenxid
+3: INSERT INTO frozen_witness SELECT datfrozenxid FROM pg_database WHERE datname = current_catalog;
+1: BEGIN;
+-- heap_update(pg_database)
+1: GRANT TEMP ON DATABASE isolation2test TO regress_temp_grantee;
+-- suspend before get 'wait gxid'
+2: SELECT gp_inject_fault('before_get_distributed_xid', 'suspend', dbid) FROM gp_segment_configuration WHERE role='p' AND content=0;
+-- inplace update
+2&: VACUUM (FREEZE);
+3: SELECT gp_wait_until_triggered_fault('before_get_distributed_xid', 1, dbid) FROM gp_segment_configuration WHERE role='p' AND content=0;
+1: COMMIT;
+3: INSERT INTO frozen_witness SELECT datfrozenxid FROM pg_database WHERE datname = current_catalog;
+3: SELECT gp_inject_fault('before_get_distributed_xid', 'reset', dbid) FROM gp_segment_configuration WHERE role='p' AND content=0;
+
+2<:
+-- Save the result in an environment variable.
+-- We get the raw xid to be sure that the age in the next query will be executed on the coordinator.
+3: @post_run 'TOKEN=`echo "${RAW_STR}" | awk \'NR==3\' | awk \'{print $1}\'` && echo ""' : SELECT min(x::varchar::int) FROM frozen_witness;
+-- observe datfrozenxid
+3: @pre_run 'echo "${RAW_STR}" | sed "s#@TOKEN#${TOKEN}#"': SELECT 'datfrozenxid retreated' FROM pg_database WHERE datname = current_catalog AND age(datfrozenxid) > age('@TOKEN'::xid);
+
+REVOKE ALL ON DATABASE isolation2test FROM regress_temp_grantee;
+DROP ROLE regress_temp_grantee;
+
+1q:
+2q:
+3q:
