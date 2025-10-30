4bd7dddc2eaead7b9788d2ba7f1cdc2abc48fdaf
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-10-30 08:41:36 +0500

    mv

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -993,22 +993,6 @@ pg_rewrite_query(Query *query)
 	return querytree_list;
 }
 
-typedef struct MotionContext
-{
-	plan_tree_base_prefix base;
-} MotionContext;
-
-static bool
-contains_motions(Node *node, void *context)
-{
-	if (node == NULL)
-		return false;
-
-	if (IsA(node, Motion))
-		return true;
-
-	return plan_tree_walker(node, contains_motions, context, true);
-}
 
 /*
  * Generate a plan for a single already-rewritten query.
@@ -1087,16 +1071,6 @@ pg_plan_query(Query *querytree, int cursorOptions, ParamListInfo boundParams)
 		elog_node_display(LOG, "plan", plan, Debug_pretty_print);
 
 	TRACE_POSTGRESQL_QUERY_PLAN_DONE();
-
-	if (Gp_role != GP_ROLE_DISPATCH)
-	{
-		MotionContext context = {.base.node = (Node*)plan};
-
-		if (contains_motions((Node *)plan->planTree, &context))
-			ereport(ERROR,
-					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-					 errmsg("function cannot execute on a QE slice because it contains motions")));
-	}
 
 	return plan;
 }
