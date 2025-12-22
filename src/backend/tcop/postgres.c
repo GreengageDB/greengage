d305934795819fbb7fda35f020a1177e5ad66082
Vladimir Sarmin <v.sarmin@arenadata.io>
2025-12-22 14:28:57 +0300

    Remove extra line

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -5031,7 +5031,6 @@ PostgresMain(int argc, char *argv[],
 										   ALLOCSET_DEFAULT_MINSIZE,
 										   ALLOCSET_DEFAULT_INITSIZE,
 										   ALLOCSET_DEFAULT_MAXSIZE);
-
 	/*
 	 * POSTGRES main processing loop begins here
 	 *
