e2fd17647c3ab6aeb7a2ace234d2501cbc4b121a
Denis Kovalev <d.kovalev@arenadata.io>
2026-01-12 22:27:30 +0300

    Fix unit tests

--- gpMgmt/bin/gppylib/test/unit/test_unit_gpsegsetuprecovery.py
+++ gpMgmt/bin/gppylib/test/unit/test_unit_gpsegsetuprecovery.py
@@ -334,7 +334,7 @@ class SegSetupRecoveryTestCase(GpTestCase):
         mock_connect.assert_called_once()
         mock_execsql.assert_called_once()
         #TODO use regex pattern
-        self.assertRegexpMatches(gplog.get_logfile(), '/gpsegsetuprecovery.py_\d+\.log')
+        self.assertRegexpMatches(gplog.get_logfile(), '/gpsegsetuprecovery.pyc?_\d+\.log')
 
     @patch('gpsegsetuprecovery.ValidationForFullRecovery.validate_failover_data_directory')
     @patch('gpsegsetuprecovery.dbconn.connect')
@@ -357,7 +357,7 @@ class SegSetupRecoveryTestCase(GpTestCase):
         mock_validate_datadir.assert_called_once()
         mock_dburl.assert_called_once()
         mock_connect.assert_called_once()
-        self.assertRegexpMatches(gplog.get_logfile(), '/gpsegsetuprecovery.py_\d+\.log')
+        self.assertRegexpMatches(gplog.get_logfile(), '/gpsegsetuprecovery.pyc?_\d+\.log')
 
 
     @patch('recovery_base.gplog.setup_tool_logging')
