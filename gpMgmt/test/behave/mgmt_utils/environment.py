0c858181908e609514489de6f57ae5068f02fde9
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-01-23 13:29:14 +0500

    fix

--- gpMgmt/test/behave/mgmt_utils/environment.py
+++ gpMgmt/test/behave/mgmt_utils/environment.py
@@ -21,7 +21,7 @@ def before_all(context):
 def before_feature(context, feature):
     # we should be able to run gpexpand without having a cluster initialized
     tags_to_skip = ['gpexpand', 'gpaddmirrors', 'gpstate',
-                    'gpconfig', 'gpssh-exkeys', 'gpstop', 'gpinitsystem', 'cross_subnet',
+                    'gpssh-exkeys', 'gpstop', 'gpinitsystem', 'cross_subnet',
                     'gplogfilter']
     if set(context.feature.tags).intersection(tags_to_skip):
         return
