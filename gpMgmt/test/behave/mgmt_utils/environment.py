005c6430209d4ee6a0b0b041dd10f3f69511f9c9
Maksim Michkov <m.michkov@arenadata.io>
2026-01-23 06:26:17 +0000

    Fix

--- gpMgmt/test/behave/mgmt_utils/environment.py
+++ gpMgmt/test/behave/mgmt_utils/environment.py
@@ -134,7 +134,7 @@ def before_scenario(context, scenario):
     if 'analyzedb' not in context.feature.tags:
         start_database_if_not_started(context)
         drop_database_if_exists(context, 'testdb')
-    if 'gp_bash_functions.sh' in context.feature.tags or 'backup_restore_bashrc' in scenario.effective_tags:
+    if 'gp_bash_functions' in context.feature.tags or 'backup_restore_bashrc' in scenario.effective_tags:
         backup_bashrc()
 
 def after_scenario(context, scenario):
