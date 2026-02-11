933ef072f42562838431731aad3442a38508dde3
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-02-11 08:22:21 +0500

    fix

--- ci/scripts/init_containers.sh
+++ ci/scripts/init_containers.sh
@@ -35,6 +35,6 @@ wait
 for service in $services
 do
   docker compose -p $project -f ci/docker-compose.yaml exec -T \
-    $service bash -c "for HOST in cdw sdw1 sdw2 sdw3 sdw4 sdw5 sdw6; do echo \"\$(host \"\$HOST\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4) \$HOST\" >>/etc/hosts; done" &
+    $service bash -c "for HOST in $services; do echo \"\$(host \"\$HOST\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4) \$HOST\" >>/etc/hosts; done" &
 done
 wait
