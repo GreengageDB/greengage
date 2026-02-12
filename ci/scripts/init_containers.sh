871d0a8c3eefee70f446fbc62fefc146a9e750a5
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-02-12 15:27:56 +0500

    optimize

--- ci/scripts/init_containers.sh
+++ ci/scripts/init_containers.sh
@@ -17,24 +17,15 @@ fi
 for service in $services
 do
   docker compose -p $project -f ci/docker-compose.yaml exec -T \
-    $service bash -c "mkdir -p /data/gpdata && chmod -R 777 /data &&
-      source gpdb_src/concourse/scripts/common.bash && install_gpdb &&
-      ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash" &
-done
-wait
-
-# Add host keys to known_hosts after containers setup
-for service in $services
-do
-  docker compose -p $project -f ci/docker-compose.yaml exec -T \
-    $service bash -c "ssh-keyscan ${services/$service/} >> /home/gpadmin/.ssh/known_hosts" &
-done
-wait
-
-# Add ip and host to /etc/hosts
-for service in $services
-do
-  docker compose -p $project -f ci/docker-compose.yaml exec -T \
-    $service bash -c "for HOST in $services; do echo \"\$(host \"\$HOST\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4) \$HOST\" >>/etc/hosts; done" &
+    $service bash -exc "
+      mkdir -p /data/gpdata;
+      chmod -R 777 /data;
+      source gpdb_src/concourse/scripts/common.bash;
+      install_gpdb;
+      ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash;
+      # Add host keys to known_hosts after containers setup
+      ssh-keyscan ${services/$service/} >> /home/gpadmin/.ssh/known_hosts;
+      # Add ip and host to /etc/hosts
+      for HOST in $services; do echo \"\$(host \"\$HOST\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4) \$HOST\" >>/etc/hosts; done" &
 done
 wait
