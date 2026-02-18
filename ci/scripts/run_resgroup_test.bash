b58252d8bb0917a15630f859983c3e10b59c77ef
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-02-18 08:52:26 +0500

    test resgroup in 6x like in 7x

--- ci/scripts/run_resgroup_test.bash
+++ ci/scripts/run_resgroup_test.bash
@@ -1,106 +1,51 @@
 #!/bin/bash
+set -eu
+LOGS=${LOGS:-$PWD/logs}
+TEST_OS=${TEST_OS:-ubuntu}
+OPTIMIZER=${OPTIMIZER:-off}
+STATEMENT_MEM=${STATEMENT_MEM:-125MB}
+
+docker run -i --user root:root \
+  -v "$LOGS":/logs \
+  -e TEST_OS="$TEST_OS" \
+  --sysctl 'kernel.sem=500 1024000 200 4096' \
+  --privileged \
+  "$IMAGE" /bin/bash << EOF
 set -eox pipefail
+exitcode=1
 
-project="resgroup"
+cd /home/gpadmin/
+ssh-keygen -A
+/usr/sbin/sshd
+source gpdb_src/concourse/scripts/common.bash
+install_and_configure_gpdb
+gpdb_src/concourse/scripts/setup_gpadmin_user.bash
+make_cluster
+chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}
+mkdir /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
+chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
+chown -R gpadmin:gpadmin /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
 
-# Exit status file for cloud-init environments where exit codes aren't propagated.
-# Parent processes can read this file to determine script success/failure.
-logdir="$PWD/logs"
-logfile=".exitcode"
+sudo -u gpadmin -- bash -c "
+  set -ex
+  source \$GPHOME/greengage_path.sh
+  source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+  make -C /home/gpadmin/gpdb_src/src/test/regress
+  make PGOPTIONS='-c optimizer=$OPTIMIZER -c statement_mem=$STATEMENT_MEM' installcheck-resgroup -C gpdb_src/
+" && exitcode=0
 
-function cleanup {
-  docker compose -p $project -f ci/docker-compose.yaml --env-file ci/.env down
-}
-
-mkdir ssh_keys "$logdir" -p
-if [ ! -e "ssh_keys/id_rsa" ]
-then
-  ssh-keygen -P "" -f ssh_keys/id_rsa
-fi
-
-trap cleanup EXIT
-
-#install gpdb and setup gpadmin user
-bash ci/scripts/init_containers.sh $project cdw sdw1
-
-for service in 'cdw' 'sdw1'
-do
-  #grant access rights to group controllers
-  docker compose -p $project -f ci/docker-compose.yaml exec -T $service bash -c "
-    chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset} &&
-    mkdir /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb &&
-    chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb &&
-    chown -R gpadmin:gpadmin /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb"
+params=(
+  "./ d gpAdminLogs"
+  "gpdb_src/src/test/ d results"
+  "gpdb_src/src/test/ f regression.diffs"
+  "gpdb_src/gpAux/gpdemo/datadirs/ d pg_log"
+)
+for param in "\${params[@]}"; do
+  read -r path type name <<< "\$param"
+  find \$path -name \$name -type \$type -exec bash -c "tar -rf '/logs/\$name.tar' '{}' ; [ '\$name' == 'regression.diffs' ] && cat '{}' || true" \;
 done
+chmod -R a+rwX /logs
 
-#create cluster
-docker compose -p $project -f ci/docker-compose.yaml exec -T cdw \
- bash -c "source gpdb_src/concourse/scripts/common.bash && HOSTS_LIST='sdw1' make_cluster"
-
-#disable exit on error to allow log collection regardless of return code
-set +e
-#run tests
-docker compose -p $project -f ci/docker-compose.yaml exec -Tu gpadmin cdw bash -ex <<EOF
-        source /usr/local/greengage-db-devel/greengage_path.sh
-        source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
-        export LDFLAGS="-L\${GPHOME}/lib"
-        export CPPFLAGS="-I\${GPHOME}/include"
-        export USER=gpadmin
-
-        cd /home/gpadmin/gpdb_src
-        ./configure --prefix=/usr/local/greengage-db-devel \
-            --without-zlib --without-rt --without-libcurl \
-            --without-libedit-preferred --without-docdir --without-readline \
-            --disable-gpcloud --disable-gpfdist --disable-orca \
-            ${CONFIGURE_FLAGS}
-
-        make -C /home/gpadmin/gpdb_src/src/test/regress
-        ssh sdw1 mkdir -p /home/gpadmin/gpdb_src/src/test/{regress,isolation2} </dev/null
-        scp /home/gpadmin/gpdb_src/src/test/regress/regress.so \
-            gpadmin@sdw1:/home/gpadmin/gpdb_src/src/test/regress/
-
-        make PGOPTIONS="-c optimizer=off" installcheck-resgroup || (
-            errcode=\$?
-            find src/test/isolation2 -name regression.diffs \
-            | while read diff; do
-                cat <<EOF1
-
-======================================================================
-DIFF FILE: \$diff
-----------------------------------------------------------------------
-
-EOF1
-                cat \$diff
-              done
-            exit \$errcode
-        )
+echo \$exitcode > /logs/.exitcode
+exit \$exitcode
 EOF
-
-# Cloud-init monitors will check for this file's existence and content.
-# Missing file or invalid content will be interpreted as script failure.
-exitcode=$?
-echo "$exitcode" > "$logdir/$logfile"
-
-docker compose -p $project -f ci/docker-compose.yaml exec -T cdw bash -ex <<EOF
-  cd /home/gpadmin
-  tar -czf /logs/gpAdminLogs.tar.gz gpAdminLogs/
-  tar -czf /logs/gpAux.tar.gz gpdb_src/gpAux/gpdemo/datadirs/gpAdminLogs/
-  tar -czf /logs/pg_log.tar.gz gpdb_src/gpAux/gpdemo/datadirs/qddir/demoDataDir-1/pg_log/ gpdb_src/gpAux/gpdemo/datadirs/standby/pg_log
-  #regression.diffs may not exist if tests were successful
-  tar --ignore-failed-read -czf /logs/results.tar.gz gpdb_src/src/test/isolation2/results/resgroup/ gpdb_src/src/test/isolation2/regression.diffs
-EOF
-
-docker compose -p $project -f ci/docker-compose.yaml exec -T sdw1 bash -ex <<EOF
-  cd /home/gpadmin
-  tar -czf /logs/gpAdminLogs.tar.gz gpAdminLogs/
-  tar -czf /logs/gpAux.tar.gz gpdb_src/gpAux/gpdemo/datadirs/gpAdminLogs/
-  tar -czf /logs/pg_log.tar.gz \
-    gpdb_src/gpAux/gpdemo/datadirs/dbfast1/demoDataDir0/pg_log \
-    gpdb_src/gpAux/gpdemo/datadirs/dbfast2/demoDataDir1/pg_log \
-    gpdb_src/gpAux/gpdemo/datadirs/dbfast3/demoDataDir2/pg_log \
-    gpdb_src/gpAux/gpdemo/datadirs/dbfast_mirror1/demoDataDir0/pg_log \
-    gpdb_src/gpAux/gpdemo/datadirs/dbfast_mirror2/demoDataDir1/pg_log \
-    gpdb_src/gpAux/gpdemo/datadirs/dbfast_mirror3/demoDataDir2/pg_log
-EOF
-
-exit "$exitcode"
