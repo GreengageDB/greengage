55cad39a0bd687c36253d99badf10fa6a47c1add
Gilles Darold <gilles@darold.net>
2026-02-19 07:42:48 +0100

    Disable the login event trigger when credcheck.password_valid_warning is not defined or set to 0.

--- gpcontrib/credcheck/event_trigger.sql
+++ gpcontrib/credcheck/event_trigger.sql
@@ -8,16 +8,21 @@ CREATE OR REPLACE FUNCTION warning_valid_until()
 $$
 DECLARE
    warn_days integer;
+   valid_warning integer;
 BEGIN
-	SELECT ((extract(epoch from valuntil) - extract(epoch from current_timestamp))/86400)::integer
-		INTO warn_days
-		FROM pg_catalog.pg_shadow
-		WHERE usename = SESSION_USER
-			AND valuntil IS NOT NULL
-			AND valuntil > current_timestamp;
-	
-	IF (warn_days IS NOT NULL AND warn_days <= current_setting('credcheck.password_valid_warning', true)::integer ) THEN
-		RAISE WARNING 'your password will expire in % days, please renew your password!', warn_days;
+	valid_warning := current_setting('credcheck.password_valid_warning', true)::integer;
+
+	IF (valid_warning IS NOT NULL AND valid_warning > 0) THEN
+		SELECT ((extract(epoch from valuntil) - extract(epoch from current_timestamp))/86400)::integer
+			INTO warn_days
+			FROM pg_catalog.pg_shadow
+			WHERE usename = SESSION_USER
+				AND valuntil IS NOT NULL
+				AND valuntil > current_timestamp;
+
+		IF (warn_days IS NOT NULL AND warn_days <= valid_warning) THEN
+			RAISE WARNING 'your password will expire in % days, please renew your password!', warn_days;
+		END IF;
 	END IF;
 END;
 $$
