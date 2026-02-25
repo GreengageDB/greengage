859e5e98ab82c80e3471be5c1fb3c6313c2bdfd2
Viktor Kurilko <v.kurilko@arenadata.io>
2026-02-25 11:36:33 +0700

    fix error handling in _update_config

--- gpMgmt/bin/gppylib/operations/buildMirrorSegments.py
+++ gpMgmt/bin/gppylib/operations/buildMirrorSegments.py
@@ -343,10 +343,9 @@ class GpMirrorListToBuild(object):
 
             self.__logger.debug("Generating configuration backout scripts")
         finally:
-            # FIXME we can silents error here. Is it expected?
             # Reenable Ctrl-C
             signal.signal(signal.SIGINT, signal.default_int_handler)
-            return backout_map
+        return backout_map
 
     def _revert_config_update(self, recovery_results, backout_map):
         if len(backout_map) == 0:
