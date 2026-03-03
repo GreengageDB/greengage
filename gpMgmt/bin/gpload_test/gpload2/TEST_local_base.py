faa50faa53d8c6e9a9a7673988c297c65e0e0ed7
Viktor Kurilko <v.kurilko@arenadata.io>
2026-03-03 21:37:12 +0700

    Fixup Python 3 support (#292)
    A few fixes for Python 3 support.
    1. Replace the remaining warn with warning.
    2. Fix the remaining row checks for emptiness.
    3. Fix the remaining six usage.
    4. Restore old dbconn connect logic to fix connection using ipv6.
    Gpload:
    1. Decode results of sub commands
    2. Fix handling of unicode
    3. Fix handling escaping
    5. Add the ability to use different ans files for different versions of Python.
    The test_76_gpload_merge_chinese_standard_conforming_str_off part of the test
    expects a string decoding error when handling an exception from DB, but Python 3
    can handle the exception text, which makes the Python 3 test result different.

--- gpMgmt/bin/gpload_test/gpload2/TEST_local_base.py
+++ gpMgmt/bin/gpload_test/gpload2/TEST_local_base.py
@@ -87,7 +87,7 @@ def run(cmd):
             valid for the second parameter of open().
     """
     p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    out = p.communicate()[0]
+    out = p.communicate()[0].decode('utf-8')
     ret = []
     ret.append(out)
     rc = False if p.wait() else True
@@ -390,7 +390,11 @@ def changeExtFile( fname, ext = ".diff", outputPath = "" ):
 
 def gpdbAnsFile(fname):
     ext = '.ans'
-    return os.path.splitext(fname)[0] + ext
+    filename = os.path.splitext(fname)[0]
+    if sys.version_info[0] == 3:
+        if os.path.isfile(filename + "_py3" + ext):
+            filename += "_py3"
+    return filename + ext
 
 def isFileEqual( f1, f2, optionalFlags = "", outputPath = "", myinitfile = ""):
     LMYD = os.path.abspath(os.path.dirname(__file__))
@@ -399,6 +403,7 @@ def isFileEqual( f1, f2, optionalFlags = "", outputPath = "", myinitfile = ""):
     if not os.access( f2, os.R_OK ):
         raise Exception( 'Error: cannot find file %s' % f2 )
     dfile = diffFile( f1, outputPath = outputPath )
+    dfile = dfile.replace("_py3", "")
     # Gets the suitePath name to add init_file
     suitePath = f1[0:f1.rindex( "/" )]
 
@@ -446,7 +451,7 @@ def copy_data(source='',target=''):
     cmd = 'cp '+ mkpath('data/' + source) + ' ' + mkpath(target)
     p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     _, err = p.communicate()
-    if err != '':
+    if err:
         sys.stderr.write(str(err))
         sys.exit(2)
 
