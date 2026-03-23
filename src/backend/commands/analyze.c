d1f83f718fb910444c88ef1bb8a150e692fce0ab
Denis Kovalev <d.kovalev@arenadata.io>
2026-03-23 08:27:56 +0300

    adjust comment

--- src/backend/commands/analyze.c
+++ src/backend/commands/analyze.c
@@ -136,8 +136,8 @@
 
 
 /*
- * For Hyperloglog, we define an error margin of 0.3%. If the number of
- * distinct values estimated by hyperloglog is within an error of 0.3%,
+ * For Hyperloglog, we define an error margin as dependency of requested standard 
+ * error. If the number of distinct values estimated by hyperloglog is within an error,
  * we consider everything as distinct.
  */
 #define GP_HLL_ERROR_MARGIN  (DEFAULT_ERROR * 2)
