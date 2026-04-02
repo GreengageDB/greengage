b1db29e8086117c7b155b1287ad69f5f410a2474
Shapatin Artem <a.shapatin@arenadata.io>
2026-04-02 20:06:55 +0700

    fix resgroup2

--- src/test/isolation2/sql/resgroup/resgroup_query_mem.sql
+++ src/test/isolation2/sql/resgroup/resgroup_query_mem.sql
@@ -12,34 +12,41 @@
 -- 5. these two ratios should be the same.
 
 create extension if not exists gp_inject_fault;
-do $$
-begin /* in func */
-  execute $func$ /* in func */
-    drop language if exists plpythonu cascade; /* in func */
-    drop language if exists plpython3u cascade; /* in func */
-    create language plpythonu; /* in func */
-  $func$; /* in func */
-exception /* in func */
-  when others then /* in func */
-    if SQLERRM = 'could not access file "$libdir/plpython2": No such file or directory' /* in func */
-    then /* in func */
-      begin /* in func */
-        execute $func$ /* in func */
-          create language plpython3u; /* in func */
-          alter language plpython3u rename to plpythonu; /* in func */
-        $func$; /* in func */
-      exception /* in func */
-      when others then /* in func */
-        raise notice 'Could not create or rename PL/Python language: %', SQLERRM; /* in func */
-      end; /* in func */
-    end if; /* in func */
-end; /* in func */
+do $$ 
+begin /*in func*/
+  execute $func$ /*in func*/
+    drop language if exists plpythonu cascade; /*in func*/
+  $func$; /*in func*/
+  begin /*in func*/
+    execute $func$ /*in func*/
+      create language plpythonu; /*in func*/
+    $func$; /*in func*/
+  exception /*in func*/
+    when others then /*in func*/
+      if SQLERRM = 'could not access file "$libdir/plpython2": No such file or directory' then /*in func*/
+        execute $func$ /*in func*/
+          drop language if exists plpython3u cascade; /*in func*/
+        $func$; /*in func*/
+        begin /*in func*/
+          execute $func$ /*in func*/
+            create language plpython3u; /*in func*/
+            alter language plpython3u rename to plpythonu; /*in func*/
+          $func$; /*in func*/
+        exception /*in func*/
+          when others then /*in func*/
+            raise exception 'Could not create or rename PL/Python language: %', SQLERRM; /*in func*/
+        end; /*in func*/
+      else /*in func*/
+        raise exception 'Failed to create PL/Pythonu: %', SQLERRM; /*in func*/
+      end if; /*in func*/
+  end; /*in func*/
+end; /*in func*/
 $$;
 
 create table t_qmem(a int);
 select gp_inject_fault('rg_qmem_qd_qe', 'skip', dbid) from gp_segment_configuration where role = 'p' and content = 0;
 
-1: create function rg_qmem_test() returns boolean as $$
+create function rg_qmem_test() returns boolean as $$
 from pygresql.pg import DB
 from copy import deepcopy
 import re
