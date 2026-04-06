8fd86fa666fcb0b07aa599fa76d3f5aa0dff90b2
Shapatin Artem <a.shapatin@arenadata.io>
2026-04-06 12:55:41 +0700

    drop plpythons language as it may confuse pg_upgrade

--- src/test/isolation2/sql/motion_socket.sql
+++ src/test/isolation2/sql/motion_socket.sql
@@ -108,3 +108,8 @@ $$ LANGUAGE plpythonu EXECUTE ON MASTER;
 
 !\retcode gpconfig -r gp_interconnect_address_type;
 !\retcode gpstop -au;
+
+-- start_ignore
+drop language if exists plpythonu cascade;
+drop language if exists plpython3u cascade;
+-- end_ignore
