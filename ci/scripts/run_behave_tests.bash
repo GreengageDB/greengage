3bf98e6d2b26970939e3f6b12d263ef405c92700
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2026-04-30 03:47:19 +0300

    upd (behave): renew scripts

--- ci/scripts/run_behave_tests.bash
+++ ci/scripts/run_behave_tests.bash
@@ -8,10 +8,13 @@ behave_tests_dir="gpMgmt/test/behave/mgmt_utils"
 
 clusters="~concourse_cluster"
 
+docker_compose_path="ci/docker-compose.yaml"
+
 if [ $# -eq 0 ]
 then
   # TODO cross_subnet and gpssh tests are excluded
-  features=`ls $behave_tests_dir -1 | grep feature | grep -v -E "cross_subnet|gpssh" | sed 's/\.feature$//'`
+  # FIXME! /home/gpadmin/sqldump/dump.sql is required for gpexpand tests
+  features=`ls $behave_tests_dir -1 | grep feature | grep -v -E "cross_subnet|gpssh|gpexpand" | sed 's/\.feature$//'`
 else
   for feature in $@
   do
@@ -43,12 +46,12 @@ run_feature() {
     local project="${feature}_demo"
   fi
   echo "Started $feature behave tests on cluster $cluster and project $project"
-  docker compose -p $project -f ci/docker-compose.yaml --env-file ci/.env up -d
+  docker compose -p $project -f "$docker_compose_path" --env-file ci/.env up -d
   # Prepare ALL containers first
-  local services=$(docker compose -p $project -f ci/docker-compose.yaml config --services | tr '\n' ' ')
+  local services=$(docker compose -p $project -f "$docker_compose_path" config --services | tr '\n' ' ')
   for service in $services
   do
-    docker compose -p $project -f ci/docker-compose.yaml exec -T \
+    docker compose -p $project -f "$docker_compose_path" exec -T \
       $service bash -c "mkdir -p /data/gpdata && chmod -R 777 /data &&
         source gpdb_src/concourse/scripts/common.bash && install_gpdb &&
        ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash" &
@@ -58,23 +61,31 @@ run_feature() {
   # Add host keys to known_hosts after containers setup
   for service in $services
   do
-    docker compose -p $project -f ci/docker-compose.yaml exec -T \
+    docker compose -p $project -f "$docker_compose_path" exec -T \
       $service bash -c "ssh-keyscan ${services/$service/} >> /home/gpadmin/.ssh/known_hosts" &
   done
   wait
 
-  docker compose -p $project -f ci/docker-compose.yaml exec -T \
+  docker compose -p $project -f "$docker_compose_path" exec -T \
     -e FEATURE="$feature" -e BEHAVE_FLAGS="--tags $feature --tags=$cluster \
-      -f behave_utils.arenadata.formatter:CustomFormatter \
+      -f behave_utils.ci.formatter:CustomFormatter \
       -o non-existed-output \
       -f allure_behave.formatter:AllureFormatter \
-      -o /tmp/allure-results"  \
+      -o /tmp/allure-results  \
+      -f pretty" \
     cdw gpdb_src/ci/scripts/behave_gpdb.bash
   status=$?
 
-  docker compose -p $project -f ci/docker-compose.yaml --env-file ci/.env down -v
+  if [ -n "$CI" ]; then
+    for service in $services; do
+      docker compose -p $project -f "$docker_compose_path" exec -T \
+        $service /bin/bash -s "$feature" < ./ci/scripts/behave_collect_logs.bash
+    done
+  fi
 
-  if [[ $status > 0 ]]; then echo "Feature $feature failed with exit code $status"; fi
+  docker compose -p $project -f "$docker_compose_path" --env-file ci/.env down -v
+
+  if [[ $status -gt 0 ]]; then echo "Feature $feature failed with exit code $status"; fi
   exit $status
 }
 
