9766714c8dafa5052f99b0d8969464d1a5f93980
Robert Gorchakov <r.gorchakov@arenadata.io>
2026-05-28 09:29:34 +0000

    Add DXL in EXPLAIN

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -2918,7 +2918,7 @@ psql_completion(const char *text, int start, int end)
  */
 	else if (Matches("EXPLAIN"))
 		COMPLETE_WITH("SELECT", "INSERT", "DELETE", "UPDATE", "DECLARE",
-					  "ANALYZE", "VERBOSE");
+					  "ANALYZE", "VERBOSE", "DXL", " (");
 	else if (HeadMatches("EXPLAIN", "(*") &&
 			 !HeadMatches("EXPLAIN", "(*)"))
 	{
@@ -2928,19 +2928,24 @@ psql_completion(const char *text, int start, int end)
 		 * one word, so the above test is correct.
 		 */
 		if (ends_with(prev_wd, '(') || ends_with(prev_wd, ','))
-			COMPLETE_WITH("ANALYZE", "VERBOSE", "COSTS", "SETTINGS",
+			COMPLETE_WITH("ANALYZE", "VERBOSE", "DXL", "COSTS", "SETTINGS",
 						  "BUFFERS", "TIMING", "SUMMARY", "FORMAT");
-		else if (TailMatches("ANALYZE|VERBOSE|COSTS|SETTINGS|BUFFERS|TIMING|SUMMARY"))
+		else if (TailMatches("ANALYZE|VERBOSE|DXL|COSTS|SETTINGS|BUFFERS|TIMING|SUMMARY"))
 			COMPLETE_WITH("ON", "OFF");
 		else if (TailMatches("FORMAT"))
 			COMPLETE_WITH("TEXT", "XML", "JSON", "YAML");
 	}
 	else if (Matches("EXPLAIN", "ANALYZE"))
 		COMPLETE_WITH("SELECT", "INSERT", "DELETE", "UPDATE", "DECLARE",
-					  "VERBOSE");
+					  "VERBOSE", "DXL");
 	else if (Matches("EXPLAIN", "(*)") ||
 			 Matches("EXPLAIN", "VERBOSE") ||
 			 Matches("EXPLAIN", "ANALYZE", "VERBOSE"))
+		COMPLETE_WITH("DXL", "SELECT", "INSERT", "DELETE", "UPDATE", "DECLARE");
+	else if (Matches("EXPLAIN", "(*)") ||
+			 Matches("EXPLAIN", "DXL") ||
+			 Matches("EXPLAIN", "ANALYZE|VERBOSE", "DXL") ||
+			 Matches("EXPLAIN", "ANALYZE", "VERBOSE", "DXL"))
 		COMPLETE_WITH("SELECT", "INSERT", "DELETE", "UPDATE", "DECLARE");
 
 /* FETCH && MOVE */
