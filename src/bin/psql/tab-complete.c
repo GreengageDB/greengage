4fa9c5c765c1bc2849b50259b7334c715db3f813
Robert Gorchakov <r.gorchakov@arenadata.io>
2026-06-01 13:13:20 +0000

    Add non trusted protocol & suggestions for functions

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -822,6 +822,22 @@ static const SchemaQuery Query_for_list_of_statistics = {
 "   FROM pg_catalog.pg_extprotocol"\
 "  WHERE substring(pg_catalog.quote_ident(ptcname),1,%d)='%s'"
 
+#define Query_for_list_of_extprotocol_readfuncs \
+" SELECT pg_catalog.quote_ident(proname)"\
+"   FROM pg_catalog.pg_proc as p" \
+"  WHERE p.prorettype = 'integer'::regtype" \
+"  AND p.pronargs = 0"\
+"  AND substring(pg_catalog.quote_ident(proname),1,%d)='%s'"
+
+#define Query_for_list_of_extprotocol_writefuncs Query_for_list_of_extprotocol_readfuncs
+
+#define Query_for_list_of_extprotocol_validatorfuncs \
+" SELECT pg_catalog.quote_ident(proname)"\
+"   FROM pg_catalog.pg_proc as p" \
+"  WHERE p.prorettype = 'void'::regtype" \
+"  AND p.pronargs = 0"\
+"  AND substring(pg_catalog.quote_ident(proname),1,%d)='%s'"
+
 #define Query_for_list_of_fdws \
 " SELECT pg_catalog.quote_ident(fdwname) "\
 "   FROM pg_catalog.pg_foreign_data_wrapper "\
@@ -2471,12 +2487,21 @@ psql_completion(const char *text, int start, int end)
 /* CREATE PROTOCOL*/
 	else if(Matches("CREATE", "TRUSTED"))
 		COMPLETE_WITH("PROTOCOL");
-	else if(Matches("CREATE", "TRUSTED", "PROTOCOL"))
+	else if(TailMatches("CREATE", "PROTOCOL") ||
+			TailMatches("CREATE", "TRUSTED", "PROTOCOL"))
 		COMPLETE_WITH_QUERY(Query_for_list_of_extprotocols);
-	else if(TailMatches("CREATE", "TRUSTED", "PROTOCOL", MatchAny))
+	else if(TailMatches("CREATE", "PROTOCOL", MatchAny) ||
+			TailMatches("CREATE", "TRUSTED", "PROTOCOL", MatchAny))
 		COMPLETE_WITH("(");
-	else if(TailMatches("CREATE", "TRUSTED", "PROTOCOL", MatchAny, "("))
-		COMPLETE_WITH("readfunc='", "writefunc='", "validatorfunc='");
+	else if(TailMatches("CREATE", "PROTOCOL", MatchAny, "(") ||
+			TailMatches("CREATE", "TRUSTED", "PROTOCOL", MatchAny, "("))
+		COMPLETE_WITH("readfunc =", "writefunc =", "validatorfunc =");
+	else if(TailMatches("readfunc", "="))
+		COMPLETE_WITH_QUERY(Query_for_list_of_extprotocol_readfuncs);
+	else if(TailMatches("writefunc", "="))
+		COMPLETE_WITH_QUERY(Query_for_list_of_extprotocol_writefuncs);
+	else if(TailMatches("validatorfunc", "="))
+		COMPLETE_WITH_QUERY(Query_for_list_of_extprotocol_validatorfuncs);
 
 /* CREATE PUBLICATION */
 	else if (Matches("CREATE", "PUBLICATION", MatchAny))
