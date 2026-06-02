6ae3732cc8f94b0135a10969a3fe641a7f34e995
Denis Kovalev <d.kovalev@arenadata.io>
2026-06-02 16:47:01 +0300

    Implement review recommendations

--- concourse/scripts/common.bash
+++ concourse/scripts/common.bash
@@ -69,7 +69,7 @@ function install_gpdb() {
 function setup_configure_vars() {
 	# We need to add GPHOME paths for configure to check for packaged
 	# libraries (e.g. ZStandard).
-	source /usr/local/greengage-db-devel/greengage_path.sh
+	source ${gpdb_bin_dir}/greengage_path.sh
 	export LDFLAGS="-L${GPHOME}/lib"
 	export CPPFLAGS="-I${GPHOME}/include"
 }
@@ -79,7 +79,7 @@ function configure() {
 	# The full set of configure options which were used for building the
 	# tree must be used here as well since the toplevel Makefile depends
 	# on these options for deciding what to test. Since we don't ship
-	su gpadmin -c "./configure --prefix=/usr/local/greengage-db-devel --with-perl --with-python --with-libxml --with-uuid=e2fs --enable-mapreduce --enable-orafce --enable-tap-tests --disable-orca --with-openssl ${CONFIGURE_FLAGS}"
+	su gpadmin -c "./configure --prefix=${gpdb_bin_dir} --with-perl --with-python --with-libxml --with-uuid=e2fs --enable-mapreduce --enable-orafce --enable-tap-tests --disable-orca --with-openssl ${CONFIGURE_FLAGS}"
 
 	popd
 }
@@ -96,17 +96,17 @@ function install_and_configure_gpdb() {
 }
 
 function make_cluster() {
-	source /usr/local/greengage-db-devel/greengage_path.sh
+	source ${gpdb_bin_dir}/greengage_path.sh
 	export BLDWRAP_POSTGRES_CONF_ADDONS=${BLDWRAP_POSTGRES_CONF_ADDONS}
 	export STATEMENT_MEM=250MB
 
 	pushd gpdb_src/gpAux/gpdemo
-	su gpadmin -c "source /usr/local/greengage-db-devel/greengage_path.sh; make create-demo-cluster WITH_MIRRORS=${WITH_MIRRORS:-true}"
+	su gpadmin -c "source ${gpdb_bin_dir}/greengage_path.sh; make create-demo-cluster WITH_MIRRORS=${WITH_MIRRORS:-true}"
 
 	if [[ "$MAKE_TEST_COMMAND" =~ gp_interconnect_type=proxy ]]; then
 		# generate the addresses for proxy mode
 		su gpadmin -c bash -- -e <<EOF
-			source /usr/local/greengage-db-devel/greengage_path.sh
+			source ${gpdb_bin_dir}/greengage_path.sh
 			source $PWD/gpdemo-env.sh
 
 			delta=-3000
