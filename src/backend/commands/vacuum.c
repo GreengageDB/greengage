d07fcde2aa35ff9d5416847f60e8b6e31e8be87d
Roman Eskin <r.eskin@arenadata.io>
2026-06-15 13:43:22 +1000

    Use ereportif

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -3157,11 +3157,11 @@ vac_update_relstats_from_list(VacuumStatsContext *stats_context)
 		if (GpPolicyIsReplicated(rel->rd_cdbpolicy))
 		{
 			Assert(stats->count >= rel->rd_cdbpolicy->numsegments);
-			elogif(stats->count > rel->rd_cdbpolicy->numsegments, WARNING,
-				   "Numsegments for relation \"%s\".\"%s\" is less than stats "
-				   "dispatch results count. Valid situation only during cluster shrink.",
-				   get_namespace_name(get_rel_namespace(stats->relid)),
-				   get_rel_name(stats->relid));
+			ereportif(stats->count > rel->rd_cdbpolicy->numsegments, WARNING,
+					  (errmsg("Numsegments for relation \"%s\".\"%s\" is less than stats "
+							  "dispatch results count. Valid situation only during cluster shrink.",
+							  get_namespace_name(get_rel_namespace(stats->relid)),
+							  get_rel_name(stats->relid))));
 
 			stats->rel_pages = stats->rel_pages / rel->rd_cdbpolicy->numsegments;
 			stats->rel_tuples = stats->rel_tuples / rel->rd_cdbpolicy->numsegments;
