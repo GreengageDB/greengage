6db7c27020cf6dc506d14aaf4c47fac0cb18c5ed
Denis Garsh <d.garsh@arenadata.io>
2026-06-23 08:42:38 +0300

    Revert "Remove foreign tables from get_rel_infos()."
    This reverts commit 29edc22449090633475adbc41474c7d88278d33b.

--- src/bin/pg_upgrade/info.c
+++ src/bin/pg_upgrade/info.c
@@ -493,6 +493,7 @@ get_rel_infos(ClusterInfo *cluster, DbInfo *dbinfo)
 			 CppAsString2(RELKIND_AOSEGMENTS) ", "
 			 CppAsString2(RELKIND_AOBLOCKDIR) ", "
 			 CppAsString2(RELKIND_MATVIEW) ", "
+			 CppAsString2(RELKIND_FOREIGN_TABLE) ", "
 			 "%s"
 			 CppAsString2(RELKIND_SEQUENCE) ") AND "
 	/* exclude possible orphaned temp tables */
