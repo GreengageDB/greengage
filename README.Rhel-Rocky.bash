0eb9d9061cedaaf023cd528620811d5cf7782b39
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2026-08-04 12:19:19 +0300

    fix(build): pin python3.9 explicitly, restore protobuf==3.20.0 pin
    python3 on Rocky 8 resolves to 3.6 which is too old;
    pin to python3.9 explicitly.
    protobuf version pin restored.

--- README.Rhel-Rocky.bash
+++ README.Rhel-Rocky.bash
@@ -13,7 +13,7 @@ dnf -y install epel-release
 export OS_VERSION="${OS_VERSION:-$(grep -oP '(?<= release )\d+' /etc/redhat-release)}"
 
 perl_packages="perl-Env perl-ExtUtils-Embed perl-IPC-Run perl-JSON perl-Test-Base"
-python_packages="python3 python3-devel python3-setuptools python3-pip python3-future"
+python_packages="python3.9 python3.9-devel python3.9-future"
 
 case "$OS_VERSION" in
     8)
