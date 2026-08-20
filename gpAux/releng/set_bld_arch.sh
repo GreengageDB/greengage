18fec85266e9710b55a8790102eade53203be154
Matvei Nelasov <m.nelasov@arenadata.io>
2026-08-20 08:19:47 +0000

    CI-6133 Add OS support

--- gpAux/releng/set_bld_arch.sh
+++ gpAux/releng/set_bld_arch.sh
@@ -4,7 +4,7 @@
 
 case "`uname -s`" in
     Linux)
-    if [ -f /etc/redhat-release -a ! -f /etc/altlinux-release ]; then
+    if [ -f /etc/redhat-release -a ! -f /etc/altlinux-release -a ! -f /etc/redos-release ]; then
         case "`cat /etc/redhat-release`" in
             *)
             BLD_ARCH_HOST="rhel`cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}'`_`uname -m | sed -e s/i686/x86_32/`"
@@ -12,10 +12,18 @@ case "`uname -s`" in
         esac
     fi
 
+    if [ -f /etc/altlinux-release ]; then
+	case "`cat /etc/altlinux-release`" in
+            *)
+            BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/' | cut -d'.' -f1,2)_$(uname -m)"
+            ;;
+       esac
+    fi
+
     if [ -f /etc/astra_version ]; then
 	case "`cat /etc/astra_version`" in
-		*)
-		BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
+            *)
+            BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
 	esac
     fi
 
