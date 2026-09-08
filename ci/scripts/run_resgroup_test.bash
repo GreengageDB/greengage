fe3ffe306e84b70fe07842e853e030b0f71c93fe
Gorchakov Robert <r.gorchakov@arenadata.io>
2026-09-08 08:27:09 +0000

    add loop devices

--- ci/scripts/run_resgroup_test.bash
+++ ci/scripts/run_resgroup_test.bash
@@ -7,9 +7,13 @@ STATEMENT_MEM=${STATEMENT_MEM:-125MB}
 
 docker run -i --user root:root \
   -v "$LOGS":/logs \
+  -v "$PWD/gpdemo-datadirs":/home/gpadmin/gpdb_src/gpAux/gpdemo/datadirs:rw \
+  -v "$PWD/testtablespace":/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace:rw \
+  -v /sys/fs/cgroup:/sys/fs/cgroup:rw \
   -e TEST_OS="$TEST_OS" \
   --sysctl 'kernel.sem=500 1024000 200 4096' \
   --privileged \
+  --cgroupns=host \
   "$IMAGE" /bin/bash << EOF
 set -eox pipefail
 exitcode=1
@@ -21,17 +25,23 @@ source gpdb_src/concourse/scripts/common.bash
 install_and_configure_gpdb
 gpdb_src/concourse/scripts/setup_gpadmin_user.bash
 make_cluster
-chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}
-mkdir /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
-chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
-chown -R gpadmin:gpadmin /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
+sudo mkdir -p /sys/fs/cgroup/gpdb /sys/fs/cgroup/gpdb.service
+sudo chmod -R 777 /sys/fs/cgroup /sys/fs/cgroup/gpdb /sys/fs/cgroup/gpdb.service
+
+dd if=/dev/zero of=/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img bs=1M count=1
+mkfs.ext4 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img
+sudo mount -o loop /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_1
+
+dd if=/dev/zero of=/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img bs=1M count=1
+mkfs.ext4 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img
+sudo mount -o loop /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_2
 
 sudo -u gpadmin -- bash -c "
   set -ex
   source \$GPHOME/greengage_path.sh
   source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
   make -C /home/gpadmin/gpdb_src/src/test/regress
-  make PGOPTIONS='-c optimizer=$OPTIMIZER -c statement_mem=$STATEMENT_MEM' installcheck-resgroup -C gpdb_src/
+  make PGOPTIONS='-c optimizer=$OPTIMIZER -c statement_mem=$STATEMENT_MEM' installcheck-resgroup-v2 -C gpdb_src/src/test/isolation2
 " && exitcode=0
 
 echo \$exitcode > /logs/.exitcode
