7cea6e3733f6450e091fdc3cad401da063d7ecdb
Robert Gorchakov <r.gorchakov@arenadata.io>
2026-09-08 13:49:16 +0300

    Finally working folder

--- ci/scripts/run_resgroup_test.bash
+++ ci/scripts/run_resgroup_test.bash
@@ -28,13 +28,18 @@ make_cluster
 sudo mkdir -p /sys/fs/cgroup/gpdb /sys/fs/cgroup/gpdb.service
 sudo chmod -R 777 /sys/fs/cgroup /sys/fs/cgroup/gpdb /sys/fs/cgroup/gpdb.service
 
-dd if=/dev/zero of=/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img bs=1M count=1
+mkdir /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_1 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_2
+
+dd if=/dev/zero of=/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img bs=1M count=64
 mkfs.ext4 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img
 sudo mount -o loop /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_1.img /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_1
 
-dd if=/dev/zero of=/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img bs=1M count=1
+dd if=/dev/zero of=/home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img bs=1M count=64
 mkfs.ext4 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img
 sudo mount -o loop /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/io_limit_fs_2.img /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_2
+
+sudo chmod -R 777 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_1 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_2
+sudo chown gpadmin:gpadmin /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_1 /home/gpadmin/gpdb_src/src/test/isolation2/testtablespace/rg_io_limit_ts_2
 
 sudo -u gpadmin -- bash -c "
   set -ex
