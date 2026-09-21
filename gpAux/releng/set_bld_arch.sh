dbb7e335964d9b59b47cdbadf886b278f7fb6866
maxim.gajdaj@gmail.com <maxim.gajdaj@gmail.com>
2026-09-21 08:54:59 +0300

    refactor(set_bld_arch.sh): drop no-op case with a single default branch

--- gpAux/releng/set_bld_arch.sh
+++ gpAux/releng/set_bld_arch.sh
@@ -6,36 +6,22 @@
 case "$(uname -s)" in
     Linux)
     if [ -f /etc/redhat-release ] && [ ! -f /etc/altlinux-release ] && [ ! -f /etc/redos-release ] && [ ! -f /etc/rocky-release ]; then
-        case "$(cat /etc/redhat-release)" in
-            *)
-            BLD_ARCH_HOST="rhel$(cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}')_$(uname -m | sed -e s/i686/x86_32/)"
-            ;;
-        esac
+        BLD_ARCH_HOST="rhel$(cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}')_$(uname -m | sed -e s/i686/x86_32/)"
     fi
 
     if [ -f /etc/altlinux-release ]; then
-	case "$(cat /etc/altlinux-release)" in
-            *)
-            BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/' | cut -d'.' -f1,2)_$(uname -m)"
-            ;;
-       esac
+        BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/' | cut -d'.' -f1,2)_$(uname -m)"
     fi
     if [ -f /etc/rocky-release ]; then
         BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}$(echo ${VERSION_ID} | cut -d. -f1)_$(uname -m))"
     fi
 
     if [ -f /etc/astra_version ]; then
-	case "$(cat /etc/astra_version)" in
-            *)
-            BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
-	esac
+        BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
     fi
 
     if [ -f /etc/redos-release ]; then
-	case "$(cat /etc/redos-release)" in
-            *)
-            BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
-	esac
+        BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
     fi
 
     if [ -z "${BLD_ARCH_HOST}" ] && [ -f /etc/os-release ]; then
