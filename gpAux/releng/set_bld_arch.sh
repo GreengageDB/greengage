b1c13f2c32e009065c31a199e490735b81cd7e08
maxim.gajdaj@gmail.com <maxim.gajdaj@gmail.com>
2026-09-21 08:52:13 +0300

    fix(set_bld_arch.sh): add sh shebang and replace obsolescent constructs

--- gpAux/releng/set_bld_arch.sh
+++ gpAux/releng/set_bld_arch.sh
@@ -1,19 +1,20 @@
+#!/bin/sh
 # ----------------------------------------------------------------------
 # Define ARCH values
 # ----------------------------------------------------------------------
 
-case "`uname -s`" in
+case "$(uname -s)" in
     Linux)
-    if [ -f /etc/redhat-release -a ! -f /etc/altlinux-release -a ! -f /etc/redos-release -a ! -f /etc/rocky-release ]; then
-        case "`cat /etc/redhat-release`" in
+    if [ -f /etc/redhat-release ] && [ ! -f /etc/altlinux-release ] && [ ! -f /etc/redos-release ] && [ ! -f /etc/rocky-release ]; then
+        case "$(cat /etc/redhat-release)" in
             *)
-            BLD_ARCH_HOST="rhel`cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}'`_`uname -m | sed -e s/i686/x86_32/`"
+            BLD_ARCH_HOST="rhel$(cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}')_$(uname -m | sed -e s/i686/x86_32/)"
             ;;
         esac
     fi
 
     if [ -f /etc/altlinux-release ]; then
-	case "`cat /etc/altlinux-release`" in
+	case "$(cat /etc/altlinux-release)" in
             *)
             BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/' | cut -d'.' -f1,2)_$(uname -m)"
             ;;
@@ -24,20 +25,20 @@ case "`uname -s`" in
     fi
 
     if [ -f /etc/astra_version ]; then
-	case "`cat /etc/astra_version`" in
+	case "$(cat /etc/astra_version)" in
             *)
             BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
 	esac
     fi
 
     if [ -f /etc/redos-release ]; then
-	case "`cat /etc/redos-release`" in
+	case "$(cat /etc/redos-release)" in
             *)
             BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID} | sed 's/-/_/')"
 	esac
     fi
 
-    if [ -z "${BLD_ARCH_HOST}" -a -f /etc/os-release ]; then
+    if [ -z "${BLD_ARCH_HOST}" ] && [ -f /etc/os-release ]; then
         BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID}_$(uname -m))"
     fi
     ;;
